f9ff95a5b6ddd8f2bb0cecf94c48fe744a6a81a1
dcversus <dcversus@gmail.com>
2019-01-20 21:04:21 +0300

    comment type-roboto

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "typeface-roboto";
+// import "typeface-roboto";
 import "./index.css";
 import App from "./App";
 import Login from "./pages/login";
