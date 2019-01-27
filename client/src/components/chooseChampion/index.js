7bcaa3c5b24ad459001a9c4cf01e8960967d0917
Danil <Danil>
2019-01-27 23:25:23 +0300

    add css custom properties and refactored commonly used styles

--- client/src/components/chooseChampion/index.js
+++ client/src/components/chooseChampion/index.js
@@ -2,20 +2,20 @@ import React from 'react'
 import './chooseChampion.css'
 import close from '../../assets/icon-close.svg'
 import avatarPlayer from '../../assets/avatarPlayer.png'
-const plays = [{ name: 's1mple' }, { name: 'coldzera' }, { name: 'NiKo' }, { name: 'dev1ce' }, { name: 'Magisk' }, { name: 'tabseN' }, { name: 'Frozen' }, { name: 'swag' }, { name: 'pimp' }, { name: 'Jame' }]
-const chooseChampion = ({ closeChoose }) => {
+const plays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const chooseChampion = () => {
   return (
     <div>
       <div className="fade" />
       <div className="addChampion">
         <h2>Choose your champion</h2>
-        <img className="closeBlock" onClick={closeChoose} src={close} alt="close icon" />
+        <img className="closeBlock" onClick={this.closeChoose} src={close} alt="close icon" />
         <form>
           <div className="players">
             {plays.map(item => (
               <div className="player-item">
                 <img src={avatarPlayer} alt="avatar player" />
-                <p>{item.name}</p>
+                <p>item.name</p>
               </div>
             ))}
           </div>
@@ -26,4 +26,4 @@ const chooseChampion = ({ closeChoose }) => {
   )
 }
 
-export default chooseChampion
+export default chooseChampion;
