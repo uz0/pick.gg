5606b546d1a4447433057b9bd58ddc67d03a24e5
Danil <Danil>
2019-01-29 02:09:20 +0300

    Created add tournament feature

--- client/src/components/newTournament/index.js
+++ client/src/components/newTournament/index.js
@@ -1,44 +1,111 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Input from '../input'
 import Select from '../select'
 import './newTournament.css'
 import close from '../../assets/icon-close.svg'
+import http from '../../services/httpService'
 
-const newTournament = ({ closeTournament }) => {
-  return (
-    <div>
-      <div className="fade" />
-      <div className="newTournament">
-        <div className="createBlock">
-          <p>
-            Create <br /> a new <br /> tournament
-          </p>
-        </div>
-        <form>
-          <img className="close-block" onClick={closeTournament} src={close} alt="close icon" />
-          <div className="wrapForm">
-            <div className="topBlock">
-              <Input label="Name" name="" placeholder="" type="text" />
-              <Select label="Tournament (from list)" option="Tournament Name" />
-              <Input label="Entry $" name="" placeholder="" type="text" />
-            </div>
-            <p>Rules</p>
-            <div className="rules-inputs">
-              <input placeholder="Cost" type="text" />
-              <input placeholder="Cost" type="text" />
-              <input placeholder="Cost" type="text" />
-              <input placeholder="Cost" type="text" />
-              <input placeholder="Cost" type="text" />
-              <input placeholder="Cost" type="text" />
-            </div>
-            <div className="bottom-btn">
-              <button onClick="">Create</button>
-            </div>
+class newTournament extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      rules: {}
+    }
+  }
+
+  onRulesInputChange = (e) => {
+    
+    let formattedInputValue = parseInt(e.target.value);
+    let value = 0;
+
+    if(formattedInputValue <= 10 && formattedInputValue >= -10){
+      value = formattedInputValue;
+    } else if (formattedInputValue >= 10) {
+      value = 10;
+    } else if (formattedInputValue <= -10) {
+      value = -10;
+    }
+
+    this.setState({
+      rules: {
+        ...this.state.rules,
+        [e.target.name]: value
+      }
+    });
+
+  }
+  
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  submitForm = async (e) => {
+    
+    e.preventDefault();
+
+    let { name, entry, rules } = this.state;
+
+    let normalizedRules = Object.keys(rules).map(item => {
+      return {
+        rule: item,
+        score: rules[item]
+      }
+    });
+
+    let request = await http('/api/tournaments', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        entry,
+        normalizedRules,
+        tournamentId: 1,
+      }),
+    });
+
+  }
+
+  render(){
+    let { closeTournament, rules } = this.props;
+    
+    return (
+      <div>
+        <div className="fade" />
+        <div className="newTournament">
+          <div className="createBlock">
+            <p>
+              Create <br /> a new <br /> tournament
+            </p>
           </div>
-        </form>
+          <form onSubmit={this.submitForm}>
+            <img className="close-block" onClick={closeTournament} src={close} alt="close icon" />
+            <div className="wrapForm">
+              <div className="topBlock">
+                <Input action={this.onChange} label="Name" name="name" type="text" />
+                <Select label="Tournament (from list)" option="Tournament Name" />
+                <Input action={this.onChange} label="Entry $" name="entry" type="text" />
+              </div>
+              <p>Rules</p>
+              <div className="rules-inputs">
+                {rules && rules.map(item => (
+                  <input name={item._id} onChange={this.onRulesInputChange} value={this.state.rules[item._id] || ''} key={item._id} placeholder={item.name} type="number" min="-10" max="10" />
+                ))}
+              </div>
+              <div className="bottom-btn">
+                <button type="submit">Create</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default newTournament
