8237b80050df1dc1c578f8d2ea87efb7910f23f1
danilstoyanov <danya.bright@yandex.ru>
2019-01-30 00:24:14 +0300

    refactored button component

--- client/src/components/button/index.js
+++ client/src/components/button/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import classnames from 'classnames/bind'
+import style from './button.module.css'
 
-const Button = ({ text, type, onClick, icon, className }) => {
+const cx = classnames.bind(style);
+
+const Button = ({ text, type, onClick, icon, className, appearance }) => {
   return (
-    <button className={className} type={type} onClick={onClick}>
-      {icon}
-      {text}
+    <button
+      className={cx('button', className)}
+      appearance={appearance}
+      type={type}
+      onClick={onClick}>
+        {icon && icon}
+        {text && <span>{text}</span>}
     </button>
   )
 }
