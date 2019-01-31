db20594563a52ab6160625800aece5b6c6848f61
danilstoyanov <danya.bright@yandex.ru>
2019-02-01 00:57:35 +0300

    Created entry and date filters

--- client/src/pages/tournaments/index.js
+++ client/src/pages/tournaments/index.js
@@ -5,29 +5,20 @@ import { NavLink } from 'react-router-dom'
 import arrow from '../../assets/arrow.svg'
 import moment from 'moment'
 import AuthService from '../../services/authService'
-import http from '../../services/httpService'
-import style from './home.module.css'
-
-// const items = [
-//   { id: '1', title: 'My Crazy League', date: 'Feb 02', users: '2023', entry: '3.97' },
-//   { id: '2', title: 'Mamkin tiger wanna play', date: 'Feb 02', users: '3052', entry: '10.00' },
-//   { id: '3', title: 'Only for hardcore fans', date: 'Feb 02', users: '1233', entry: '2.17' },
-//   { id: '4', title: 'Crazy camp', date: 'Feb 02', users: '1652', entry: '1.00' },
-//   { id: '5', title: 'Lvl up', date: 'Feb 03', users: '1054', entry: '5.35' },
-//   { id: '6', title: 'Super win tonight', date: 'Feb 03', users: '2582', entry: '4.70' },
-//   { id: '7', title: 'Mega HARDDD', date: 'Feb 03', users: '1629', entry: '1.30' },
-//   { id: '8', title: 'Whats happened? AAAA!', date: 'Feb 03', users: '254', entry: '9.09' },
-//   { id: '9', title: 'AAAAAAAAAAAAA!!!', date: 'Feb 03', users: '4346', entry: '7.77' },
-// ]
+import TournamentService from '../../services/tournamentService'
+import style from './tournaments.module.css'
 
 class App extends Component {
   constructor() {
     super()
     this.AuthService = new AuthService()
+    this.TournamentService = new TournamentService(); 
     this.state = {
       newTournament: false,
       tournaments: [],
       rules: [],
+      entryFilter: '',
+      dateFilter: '',
     }
   }
   createTournament = () =>
@@ -40,20 +31,58 @@ class App extends Component {
       newTournament: false,
     })
 
-  async componentDidMount() {
-    let tournamentsQuery = await http('/api/tournaments')
-    let rulesQuery = await http('/api/rules')
+  filterByDate = async(e) => {
 
-    let tournaments = await tournamentsQuery.json()
-    let rules = await rulesQuery.json()
-
-    this.setState(
-      {
+    if(e.target.value === ''){
+      let tournaments = await this.TournamentService.getAllTournaments();
+      this.setState({
         tournaments: tournaments.tournaments,
-        rules: rules.rules,
-      },
-      () => console.log(this.state),
-    )
+        dateFilter: ''
+      })
+      return
+    }
+
+    this.setState({
+      dateFilter: e.target.value,
+    }, async () => {
+      let filteredTournaments = await this.TournamentService.filterTournamentsByDate(this.state.dateFilter);
+      this.setState({ tournaments: filteredTournaments })
+    })
+
+  }
+
+  filterByEntry = async(e) => {
+
+    if(e.target.value <= 0){
+      let tournaments = await this.TournamentService.getAllTournaments();
+      this.setState({
+        tournaments: tournaments.tournaments,
+        entryFilter: ''
+      })
+      return
+    }
+
+    this.setState({
+      entryFilter: e.target.value,
+    }, async () => {
+      let filteredTournaments = await this.TournamentService.filterTournamentsByEntry(this.state.entryFilter);
+      this.setState({ tournaments: filteredTournaments })
+    })
+
+  } 
+
+  async componentDidMount() {
+
+    let tournaments = await this.TournamentService.getAllTournaments();
+  
+    let rulesQuery = await http('/api/rules');
+    let rules = await rulesQuery.json();
+
+    this.setState({
+      tournaments: tournaments.tournaments,
+      rules: rules.rules,
+    });
+
   }
 
   render() {
@@ -63,8 +92,22 @@ class App extends Component {
         <div className={style.filters}>
           <h2>Tournaments</h2>
           <form>
-            <Input label="End date" name="date" type="date" />
-            <Input label="Minimal entry" name="entry" placeholder="$ 0.1" type="text" />
+            <Input
+              type="date"
+              value={this.state.filterByDate}
+              action={this.filterByDate}
+              label="End date"
+              name="date"
+            />
+            <Input
+              type="number"
+              value={this.state.entryFilter}
+              action={this.filterByEntry}
+              label="Minimal entry"
+              placeholder="$ 0.1"
+              name="entry"
+              min="0"
+            />
           </form>
           <div className={style.create_tournament}>
             <p>Not satisfied?</p>
