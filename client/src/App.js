fe34c636580f0ce926146e84253df430e4937afa
danilstoyanov <danya.bright@yandex.ru>
2019-02-06 20:03:33 +0300

    created profile page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import TopMenuComponent from './components/TopMenuComponent'
 import Tournaments from './pages/tournaments'
 import Tournament from './pages/tournament'
+import Profile from './pages/profile'
 
 const App = ({ history }) => (
 	<div>
@@ -11,6 +12,7 @@ const App = ({ history }) => (
 			<Switch>
 				<Route exact path="/tournaments" component={Tournaments} />
 				<Route exact path="/tournaments/:id" component={Tournament} />
+				<Route exact path="/profile" component={Profile} />
 				<Redirect to="/tournaments"/>
 			</Switch>
 		</Container>
