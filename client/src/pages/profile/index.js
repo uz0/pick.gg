90f184b0ec3bc831d6ffb62a230587a59069e658
danilstoyanov <danya.bright@yandex.ru>
2019-02-06 20:03:33 +0300

    created profile page

---
+++ client/src/pages/profile/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import AuthService from '../../services/authService'
+import TournamentService from '../../services/tournamentService'
+import style from './profile.module.css'
+
+class Profile extends Component {
+  constructor() {
+    super()
+    this.AuthService = new AuthService()
+    this.TournamentService = new TournamentService(); 
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className={style.home_page}>
+        <div className={style.bg_wrap} />
+        {'Мой профайл!'}
+      </div>
+    )
+  }
+}
+
+export default Profile
