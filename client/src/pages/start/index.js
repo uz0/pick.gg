ad026399c0cdb12b509fc585a0103e98914a6d4f
danilstoyanov <danya.bright@yandex.ru>
2019-02-08 00:11:46 +0300

    start pages minor fixes

--- client/src/pages/start/index.js
+++ client/src/pages/start/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
-import style from '../../components/style.module.css'
+import style from './style.module.css'
 import AuthService from '../../services/authService'
 
 class Start extends Component {
