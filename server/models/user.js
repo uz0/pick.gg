857816c0cd05ea272cfaca0ca82427de6642e3b6
danilstoyanov <danya.bright@yandex.ru>
2019-02-12 00:29:46 +0300

    working on user profile

--- server/models/user.js
+++ server/models/user.js
@@ -4,5 +4,7 @@ let Schema = mongoose.Schema;
 export default mongoose.model('User', new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  about: { type: String },
+  email: { type: String },
   isAdmin : { type: Boolean },
 }));
