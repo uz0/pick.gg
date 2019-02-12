48eeb53848ece34a3f798f1d97792bf5cb59355e
danilstoyanov <danya.bright@yandex.ru>
2019-02-12 21:12:20 +0300

    created getAllUsers method and replaced faked date in ratings with real

--- client/src/pages/rating/index.js
+++ client/src/pages/rating/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { ReactComponent as AvatarPlaceholder } from '../../assets/avatar-placeholder.svg'
 import AuthService from '../../services/authService'
 import TournamentService from '../../services/tournamentService'
+import UserService from '../../services/userService'
 import { NavLink } from 'react-router-dom'
 import uuid from 'uuid'
 import style from './style.module.css'
@@ -9,25 +11,29 @@ class Rating extends Component {
   constructor() {
     super()
     this.AuthService = new AuthService()
-    this.TournamentService = new TournamentService(); 
+    this.UserService = new UserService()
+    this.TournamentService = new TournamentService()
     this.state = {
-        arrPlayers: [
-          {number: 1, name: "Painkiller", percent: 2.4, img_url: "http://download.seaicons.com/icons/bad-blood/yolks-2/256/eyes-on-fire-icon.png"},
-          {number: 2, name: "Felix", percent: 2.1, img_url:"http://www.avatar-mix.ru/avatars_low_200x200/1427.jpg"},
-          {number: 3, name: "Gart",percent: 1.9, img_url:"https://yasdnepra.com/download/file.php?avatar=16801_1514274042.png"},
-          {number: 4, name: "Mario",percent: 1.8, img_url:"http://avatarbox.net/avatars/img26/south_park_chef_avatar_picture_62880.png"},
-          {number: 5, name: "Jack M.",percent: 1.5, img_url:"https://s.gravatar.com/avatar/c5b6bf542ec004619ea119551ed47998?size=100&default=retro"},
-          {number: 6, name: "Mr. D",percent: 1.4, img_url:"https://wf.mail.ru/forums/image.php?u=1339510&dateline=1468449541"},
-          {number: 7, name: "NetSky",percent: 1.2, img_url:"https://iknowyourmeme.files.wordpress.com/2016/04/6ee.jpg?w=616"},
-          {number: 8, name: "Zzz",percent: 1.1, img_url:"http://www.avatarsdb.com/avatars/panda_kiss.gif"},
-          {number: 9, name: "Navi",percent: 1, img_url:"http://rs891.pbsrc.com/albums/ac113/Onigiri_Iconz/Fairy%20Tail/Happy/6.png~c200"},
-        ],
+      playersList: [],
     }
   }
-  
-  handleChange = () => {}
 
+  componentDidMount = async() => {
+    let playersList = await this.UserService.getAllUsers();
+
+    //loop through list and add place prop
+    let playersListWithPlaces = playersList.users.map((item, index) => {
+      item.place = index + 1;
+      return item;
+    })
+
+    this.setState({ playersList: playersListWithPlaces })
+  }
+  
   render() {
+
+    let Avatar = () => this.props.avatar ? <img src={this.props.avatar} alt="userpic"/> : <AvatarPlaceholder />;
+
     return (
       <div className={style.home_page}>
         <div className={style.bg_wrap} />
@@ -39,13 +45,14 @@ class Rating extends Component {
               <div className={style.name_header}>Name</div>
               <div className={style.percent_header}>%</div>
             </div>
-            {this.state.arrPlayers.map(item => (
-              <NavLink key={uuid()} className={style.item_table} to="/user/1">
-              <div>{item.number}.</div>
-              <div className={style.avatar_table}><img src={item.img_url}/></div>
-              <div className={style.name_table}>{item.name}</div>
-              <div className={style.percent_table}>{item.percent}</div>
-            </NavLink>
+            {this.state.playersList.map(item => (
+              <NavLink key={uuid()} className={style.item_table} to={`/user/${item._id}`}>
+                <div>{item.place}.</div>
+                <div className={style.avatar_table}>
+                  <Avatar avatar=""/>
+                </div>
+                <div className={style.name_table}>{item.username}</div>
+              </NavLink>
             ))}
           </div>
         </main>
