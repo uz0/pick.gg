461b9df2086d50d9ee26251cab859343ac8241e9
danilstoyanov <danya.bright@yandex.ru>
2019-02-12 21:12:20 +0300

    created getAllUsers method and replaced faked date in ratings with real

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -2,6 +2,12 @@ import http from './httpService'
 
 export default class UserService {
 
+  getAllUsers = async() => {
+    let usersQuery = await http('/api/users');
+    let users = await usersQuery.json();
+    return users;
+  }
+
   getMyProfile = async() => {
     let userQuery = await http('/api/users/me');
     let user = await userQuery.json();
