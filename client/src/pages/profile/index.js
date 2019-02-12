603e7f362232ebe093e210a8195dbfa93d221bd0
Ivan <kot91ivan@gmail.com>
2019-02-13 00:10:17 +0300

    Show username and email in input

--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import AuthService from '../../services/authService'
 import TournamentService from '../../services/tournamentService'
+import UserService from '../../services/userService'
 import Button from '../../components/button'
 import ProfileSidebar from '../../components/ProfileSidebar'
 import style from './profile.module.css'
@@ -9,17 +10,28 @@ class Profile extends Component {
   constructor() {
     super()
     this.AuthService = new AuthService()
+    this.UserService = new UserService()
     this.TournamentService = new TournamentService(); 
     this.state = {
-      nick: "",
+      username: "",
       email: "",
       about: "",
+
     }
   }
 
   handleChange = () => {}
 
+  async componentDidMount(){
+    let username = await this.UserService.getMyProfile()
+    
+    this.setState({ username: username.user })
+
+
+  }
+  
   render() {
+    console.log(this.state.username)
     return (
       <div className={style.home_page}>
         <div className={style.bg_wrap} />
@@ -30,12 +42,13 @@ class Profile extends Component {
             <div className={style.form_container}>
               <form className={style.form}>
                 <div>
-                  <label>Nickname</label>
-                  <input type="text" name="nickname" onChange={this.handleChange}  />
+                  <label>Username</label>
+                  <input type="text" name="username" disabled value={this.state.username.username} />
+                  
                 </div>
                 <div>
                   <label>Email</label>
-                  <input type="text" name="email" onChange={this.handleChange}  />
+                  <input type="text" name="email" value={this.state.username.email} onChange={this.handleChange}  />
                 </div>
                 <div>
                   <label>About</label>
