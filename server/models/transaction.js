ba2a3456d8246b34466797bcfbf68a2bb00c7fce
danilstoyanov <danya.bright@yandex.ru>
2019-02-18 20:44:13 +0300

    add transaction origin field to transaction model (back-end)

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -5,5 +5,6 @@ export default mongoose.model('Transactions', new Schema({
   userId: { type: String, required: true },
   amount: { type: Number, required: true },
   operation: { type: String, enum: ['deposit', 'withdraw'], required: true },
+  origin: { type: String, enum: ['user', 'tournament'] },
   date: { type: Date, required: true }
 }));
