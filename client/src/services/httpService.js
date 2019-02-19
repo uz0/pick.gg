9697038019151fa20dfafde7d6ef2223289f0b70
danilstoyanov <danya.bright@yandex.ru>
2019-02-19 23:14:20 +0300

    add baseUrl into http service

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,8 +1,9 @@
 import AuthService from '../services/authService';
 
 let authService = new AuthService();
+let baseUrl = 'http://localhost:3000';
 
-export default (url, options = {}) => fetch(url, {
+export default (url, options = {}) => fetch(`${baseUrl}${url}`, {
   ...options,
   headers: {
     ...options.headers,
