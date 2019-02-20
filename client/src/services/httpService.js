efa40b47931bc0e0eaead232bcf6be167d97457e
danilstoyanov <danya.bright@yandex.ru>
2019-02-20 20:45:36 +0300

    updated base url in http service

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,7 +1,7 @@
 import AuthService from '../services/authService';
 
 let authService = new AuthService();
-let baseUrl = 'http://localhost:3000';
+let baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://pick.gg';
 
 export default (url, options = {}) => fetch(`${baseUrl}${url}`, {
   ...options,
