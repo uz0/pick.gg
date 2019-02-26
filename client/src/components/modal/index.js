98c8f13146fc4d182a5850f6fe34267dc26f6960
Ivan <kot91ivan@gmail.com>
2019-02-26 16:00:42 +0300

    add modal component

--- client/src/components/modal/index.js
+++ client/src/components/modal/index.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import style from './modal.module.css'
+import { ReactComponent as CloseIcon } from '../../assets/close.svg'
 import Button from '../button'
 
 class Modal extends Component {
  render(){
+   let {textModal, closeModal} = this.props
    return (
-    <div className="wrapper">
-      <div className="modal">
-        <Button 
-          className={style.close}
-          onClick={this.props.close}
-        ></Button>
+    <div className={style.wrapper_modal}>
+      <div className={style.modal}>
+        <Button className={style.close_button} appearance={'_icon-transparent'} icon={<CloseIcon />} onClick={closeModal} />
+        <p>{textModal}</p>
+        <div className={style.btns_modal}>
+          <Button appearance={'_basic-accent'} type={'submit'} text={'Yes'} />
+          <Button appearance={'_basic-accent'} onClick={closeModal} text={'No'} />
+        </div>
       </div>
     </div>
+   )
  }
 }
+export default Modal
