b8dda148009e3f9313e671a6dd372921916909bb
danilstoyanov <danya.bright@yandex.ru>
2019-02-27 18:19:14 +0300

    select component updates

--- client/src/components/select/index.js
+++ client/src/components/select/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Select = ({ label, name, placeholder, type, autofocus, value, action, className, option }) => {
+const Select = ({ label, tournamentsData, name, placeholder, type, autofocus, value, action, className, option }) => {
   return (
     <label className="inputComponent">
       <p className="labelComponent">{label}</p>
 
-      <select name={name} placeholder={placeholder} className={className} type={type} autoFocus={autofocus} value={value} onChange={action}>
-        <option>{option}</option>
+      <select name={name} placeholder={placeholder} className={className} type={type} autoFocus={autofocus} value={value} onChange={action} required>
+        <option hidden disabled selected value></option>
+        {tournamentsData && tournamentsData.map(item =><option key={item._id}>{item.name}</option>)}
       </select>
     </label>
   )
