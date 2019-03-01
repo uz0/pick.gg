623f62861a47c835a45767f78fc7f5d611554dc8
danilstoyanov <danya.bright@yandex.ru>
2019-03-01 20:57:38 +0300

    fixed class and className bug

--- client/src/components/preloader/index.js
+++ client/src/components/preloader/index.js
@@ -4,8 +4,8 @@ import style from './preloader.module.css'
 class Preloader extends Component {
   render() {
     return (
-      <div class={style.wrapper}>
-        <div class={style.loader_circle}></div>
+      <div className={style.wrapper}>
+        <div className={style.loader_circle}></div>
       </div>
     )
   }
