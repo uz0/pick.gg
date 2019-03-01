f00b8db6273908829cd447d8988a999c1fcad91f
Ivan <kot91ivan@gmail.com>
2019-03-01 17:49:55 +0300

    edit front

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+
 import AuthService from '../../services/authService'
+
 import Input from '../../components/input'
 import Button from '../../components/button'
 import style from '../../components/style.module.css'
@@ -8,7 +10,9 @@ import style from '../../components/style.module.css'
 class Login extends Component {
   constructor() {
     super()
+
     this.auth = new AuthService()
+
     this.handleChange = this.handleChange.bind(this)
     this.handleLogin = this.handleLogin.bind(this)
     this.state = {
@@ -26,18 +30,22 @@ class Login extends Component {
     e.preventDefault()
     let success = await this.auth.login(this.state.username, this.state.password)
     if (success) this.props.history.replace('/tournaments')
+    
   }
 
   componentWillMount() {
     if (this.auth.isLoggedIn()) this.props.history.replace('/')
+    
   }
 
   render() {
     return (
       <div className={style.login_page}>
         <div className={style.bg_wrap} />
+
         <div className={style.form_block}>
-          <div className={style.info_block}>Please login in the system</div>
+          <div className={style.info_block}>Login</div>
+
           <form onSubmit={this.handleLogin}>
             <Input id="username" label="Email" name="username" placeholder="login" type="text" autofocus={true} value={this.state.username} action={this.handleChange} />
             <Input id="password" label="Password" name="password" placeholder="password" type="password" value={this.state.password} action={this.handleChange} />
