087c34b5379d854a1753e9ce4e940754711ed292
danilstoyanov <danya.bright@yandex.ru>
2019-03-10 23:36:07 +0300

    add guide section to the index page

--- client/src/pages/start/index.js
+++ client/src/pages/start/index.js
@@ -3,6 +3,13 @@ import { NavLink } from 'react-router-dom';
 import style from './style.module.css';
 import AuthService from '../../services/authService';
 
+import tournamentsList from '../../assets/faq/tournaments_list.png';
+import tournamentsFinished from '../../assets/faq/tournament_finished.png';
+import championsCards from '../../assets/faq/champions.png';
+
+import classnames from 'classnames';
+const cx = classnames.bind(style);
+
 class Start extends Component {
   constructor() {
     super();
@@ -33,22 +40,56 @@ class Start extends Component {
     let startButtonLink = localStorage.getItem('JWS_TOKEN') ? "/tournaments" : "/login";
     return (
       <div className={style.login_page}>
-        <div className={style.bg_wrap} />
-        
-        <div className={style.start_content}>
-          <h1>Fantasy league</h1>
+
+        <section className={style.login_section}>
+          <div className={style.bg_wrap} />
           
-          <div className={style.start_btns}>
-            <NavLink to={startButtonLink}>
-              <button>Start</button>
-            </NavLink>
+          <div className={style.start_content}>
+            <h1>Fantasy league</h1>
             
-            <div>
-              <span>or </span>
-              <NavLink to="/register">register</NavLink>
+            <div className={style.start_btns}>
+              <NavLink to={startButtonLink}>
+                <button>Start</button>
+              </NavLink>
+              
+              <div>
+                <span>or </span>
+                <NavLink to="/register">register</NavLink>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+
+        <section className={style.guide}>
+          <div className={style.step}>
+            <div>
+              <h2>1. Choose fantasy tournament</h2>
+              <p>Choose fantasy tournament you would like to take part in.</p>
+            </div>
+            <div>
+              <img src={tournamentsList} alt="Tournaments list"/>
+            </div>
+          </div>
+          <div className={cx(style.step, style.align_right)}>
+            <div>
+              <h2>2. Create your dream team</h2>
+              <p>Pick 5 players from the players list.</p>
+            </div>
+            <div>
+              <img src={championsCards} alt="User team"/>
+            </div>
+          </div>
+          <div className={style.step}>
+            <div>
+              <h2>3. Win money</h2>
+              <p>Pick 5 players from the players list.</p>
+            </div>
+            <div>
+              <img src={tournamentsFinished} alt="Tournament result"/>
+            </div>
+          </div>
+        </section>
+
       </div>
     );
   }
