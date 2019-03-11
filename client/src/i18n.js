d77a2664e2a8c998c2dcfe0809f689d42d2c2594
Ivan <kot91ivan@gmail.com>
2019-03-12 00:01:22 +0200

    add translate on start, login and register

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,12 +1,12 @@
 import i18n from 'i18next';
-import { en, ru } from '../i18n';
+import { en, ru } from './i18';
 import { reactI18nextModule } from 'react-i18next';
 
 i18n.use(reactI18nextModule).init({
   debug    : true,
-  lng      : 'en',
+  lng      : 'ru',
   react    : {defaultTransParent: 'div', wait: true},
-  resources: {en, ru},
+  resources: { en, ru },
 });
 
 export default i18n;
