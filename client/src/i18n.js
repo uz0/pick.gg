a98393b52c8dded5408aad04aa602fd78465af14
Roman <efim1382@gmail.com>
2019-03-12 15:36:11 +0200

    update version

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,11 +1,9 @@
 import i18n from 'i18next';
 import { en, ru } from './i18';
-import { reactI18nextModule } from 'react-i18next';
 
-i18n.use(reactI18nextModule).init({
+i18n.init({
   debug    : true,
   lng      : 'en',
-  react    : {defaultTransParent: 'div', wait: true},
   resources: { en, ru },
 });
 
