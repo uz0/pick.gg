780076338c8aa9efd964db73cbb0fc17d51e127a
Roman <efim1382@gmail.com>
2019-03-15 17:09:03 +0200

    set email as required

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ export default mongoose.model('User', new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   about: { type: String },
-  email: { type: String },
+  email: { type: String, unique: true },
   balance: { type: Number },
   isAdmin : { type: Boolean },
 }));
