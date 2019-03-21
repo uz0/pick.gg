bc09d2a613850ce4f88f243a2447ad22f9594dad
danilstoyanov <danya.bright@yandex.ru>
2019-03-22 01:12:45 +0300

    champions add / update / delete feature

--- server/models/player.js
+++ server/models/player.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 export default mongoose.model('Player', new Schema({
-  id: { type: Number, required: true },
+  // id: { type: Number, required: true },
   name: { type: String, required: true },
   photo: { type: String },
 }));
