3156fdd9092b4b4eee4dafc0863ab4462fe3be60
danilstoyanov <danya.bright@yandex.ru>
2019-03-24 19:15:09 +0300

    models updates

--- server/models/tournament.js
+++ server/models/tournament.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 let schema = new Schema({
-  // id: { type: Number, required: true },
+  id: { type: Number },
   name: { type: String, required: true },
   date: { type: Date, required: true },
   champions: [{ type: Schema.Types.ObjectId, ref: 'Player' }],
-  // champions_ids: [Number],
-  // matches_ids: [Number],
+  champions_ids: [Number],
+  matches_ids: [Number],
 },
 // {
 //   toObject: {virtuals:true},
