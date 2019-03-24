2a91fb7cd9219c134e29ab8de468aebc35822656
danilstoyanov <danya.bright@yandex.ru>
2019-03-24 19:15:09 +0300

    models updates

--- server/models/match.js
+++ server/models/match.js
@@ -2,20 +2,23 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 let schema = new Schema({
-  id: { type: Number, required: true },
-  tournament_id: { type: Number, required: true },
+  id: { type: Number },
+  tournament_id: { type: Number },
+  tournament: { type: Schema.Types.ObjectId, ref: 'tournament' }, // Временное поле для создания матча через админку 
   startDate: { type: Date, required: true },
   results: { type: Schema.Types.ObjectId, ref: 'MatchResult' },
   completed: Boolean,
-}, {
-  toObject: {virtuals:true},
-});
+}, 
+// {
+//   toObject: {virtuals:true},
+// }
+);
 
-schema.virtual('tournament', {
-  ref: 'Tournament',
-  localField: 'tournament_id',
-  foreignField: 'id',
-  justOne: true,
-});
+// schema.virtual('tournament', {
+//   ref: 'Tournament',
+//   localField: 'tournament_id',
+//   foreignField: 'id',
+//   justOne: true,
+// });
 
 export default mongoose.model('Match', schema);
