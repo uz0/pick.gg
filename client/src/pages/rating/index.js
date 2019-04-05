24e55aa0643d7cd46d6ac9c68ec43f1b81d66add
Ivan <kot91ivan@gmail.com>
2019-04-05 15:10:37 +0300

    Change Table for Rating page

--- client/src/pages/rating/index.js
+++ client/src/pages/rating/index.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { ReactComponent as AvatarPlaceholder } from '../../assets/avatar-placeholder.svg';
-import Preloader from '../../components/preloader';
+import { ReactComponent as AvatarPlaceholder } from 'assets/avatar-placeholder.svg';
+import Preloader from 'components/preloader';
+import Table from 'components/table';
 
-import UserService from '../../services/userService';
+import UserService from 'services/userService';
 
-import uuid from 'uuid';
 import style from './style.module.css';
 import i18n from 'i18n';
+
+const ratingTableCaptions = {
+  place: {
+    text: '#',
+    width: 10,
+  },
+
+  // avatar: {
+  //   text: '',
+  //   width: 100,
+  // },
+
+  username: {
+    text: i18n.t('username'),
+    width: 300,
+  },
+
+  winning: {
+    text: i18n.t('amount'),
+    width: 200,
+  },
+};
 
 class Rating extends Component {
   constructor() {
@@ -25,45 +47,62 @@ class Rating extends Component {
       loader: false,
     })
 
-  componentDidMount = async() => {
+  componentDidMount = async () => {
     let rating = await this.userService.getUsersRating();
     rating.rating.forEach((item, index) => item.place = index + 1);
 
     this.setState({ playersList: rating.rating });
     this.preloader();
   }
-  
+
+  renderRow = ({ className, itemClass, textClass, item }) => {
+    let Avatar = () => this.props.photo ? <img src={this.props.photo} alt="userpic" /> : <AvatarPlaceholder />;
+    { console.log(item) }
+    return <NavLink to={`/user/${item._id}`} className={className} key={item._id}>
+      <div className={itemClass} style={{ '--width': ratingTableCaptions.place.width }}>
+        <span className={textClass}>{item.place}</span>
+      </div>
+
+      {/* <div className={itemClass} style={{ '--width': ratingTableCaptions.avatar.width }}>
+        <span className={textClass}><AvatarPlaceholder/></span>
+      </div> */}
+
+      <div className={itemClass} style={{ '--width': ratingTableCaptions.username.width }}>
+        <span className={textClass}>{item.username}</span>
+      </div>
+
+      <div className={itemClass} style={{ '--width': ratingTableCaptions.winning.width }}>
+        <span className={textClass}>${item.winning}</span>
+      </div>
+    </NavLink>;
+  }
+
   render() {
-    let Avatar = () => this.props.avatar ? <img src={this.props.avatar} alt="userpic"/> : <AvatarPlaceholder />;
+    let Avatar = () => this.props.photo ? <img src={this.props.photo} alt="userpic" /> : <AvatarPlaceholder />;
     return (
       <div className={style.home_page}>
         {this.state.loader && <Preloader />}
 
         <main className={style.main_block}>
           <h1>{i18n.t('best_players')}</h1>
-          
-          <div className={style.content}>
-            <div className={style.header_table}>
-              <div className={style.number_header}>#</div>
-              <div className={style.name_header}>{i18n.t('name')}</div>
-              <div className={style.percent_header}>$</div>
-            </div>
 
-            {this.state.playersList.map(item => (
-              <NavLink key={uuid()} className={style.item_table} to={`/user/${item._id}`}>
-                <div>{item.place}.</div>
-                
-                <div className={style.avatar_table}>
-                  <Avatar avatar=""/>
-                </div>
-                
-                <div className={style.name_table}>{item.username}</div>
-                <div className={style.name_table}>${item.winning}</div>
-              </NavLink>
-            ))}
+          <div className={style.section}>
+            <Table
+              captions={ratingTableCaptions}
+              defaultSorting={this.tournamentsDefaultSorting}
+              items={this.state.playersList}
+              className={style.table}
+              renderRow={this.renderRow}
+              isLoading={this.state.isLoading}
+              emptyMessage={i18n.t('there_is_no_tournaments_yet')}
+            />
           </div>
+
+          {this.state.isLoading &&
+            <Preloader />
+          }
         </main>
-      </div>
+      </div >
     );
   }
 }
