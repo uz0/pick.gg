b7ded3d9965d92ad84d94e8cd8e3c80f6da36517
danilstoyanov <danya.bright@yandex.ru>
2019-04-17 00:21:21 +0300

    notification-bell counter update

--- client/src/components/notification/notification-bell/index.js
+++ client/src/components/notification/notification-bell/index.js
@@ -8,13 +8,15 @@ const cx = classnames.bind(style);
 class NotificationBell extends Component {
   state = {
     isActive: false,
-    notificationCount: 0,
+    notificationCount: 12,
   }
 
   render() {
+    const counterValue = this.state.notificationCount < 10 ? this.state.notificationCount : '9+';
+
     return (
       <div className={style.notification}>
-        <span className={style.counter}>12</span>
+        <span className={style.counter}>{counterValue}</span>
         <i className="material-icons">notifications</i>
       </div>
     );
