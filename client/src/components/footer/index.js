9e2b94034d9c026ffddb03cd900f414c57ded6fc
Ivan <kot91ivan@gmail.com>
2019-04-17 18:33:04 +0300

    add footer change lng

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,6 +1,7 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import AuthWrapper from '../authWrapper';
+import { ReactComponent as DiscordIcon } from 'assets/icon-discord.svg';
 
 import style from './style.module.css';
 import i18n from 'i18n';
@@ -19,9 +20,24 @@ class TopMenuComponent extends Component {
     return (
       <div className={style.footer}>
         <div className={style.footer_wrap}>
-          <p>© 2019 uz0</p>
+          <div className={style.change_lng}>
+            <NavLink to="/ru">RU</NavLink>
+            <NavLink to="/en">EN</NavLink>
+          </div>
 
-          <NavLink to="#">{i18n.t('terms_and_agreement')}</NavLink>
+          <div className={style.contact}>
+            <p>Contact as:</p>
+
+            <NavLink to="#">
+              <DiscordIcon />
+            </NavLink>
+          </div>
+
+          <div className={style.info_footer}>
+            <p>© 2019 uz0</p>
+
+            <NavLink to="#">{i18n.t('terms_and_agreement')}</NavLink>
+          </div>
         </div>
       </div>
     );
