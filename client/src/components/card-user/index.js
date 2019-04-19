2ec4b59a7ec3acddc562effca2588097cbc9d009
Ivan <kot91ivan@gmail.com>
2019-04-19 16:12:59 +0300

    Add skeleton for rating and transactions

---
+++ client/src/components/card-user/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import style from './style.module.css';
+
+const cx = classnames.bind(style);
+
+const Card = ({
+  items,
+  renderCard,
+  defaultSorting,
+  className,
+}) => {
+  let list = items;
+
+  if (defaultSorting) {
+    list.sort(defaultSorting);
+  }
+
+  return <div className={cx('card_block', className)}>
+
+    {list.length > 0 &&
+      list.map((item, index) => renderCard({
+        item,
+        index,
+        className: style.card,
+        nameClass: style.name,
+        avatarClass: style.avatar,
+        winningsClass: style.winnings,
+      }))
+    }
+
+  </div>;
+};
+
+export default Card;
