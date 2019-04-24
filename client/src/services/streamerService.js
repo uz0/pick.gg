4960214b1ad3f140941dd4cfafddba7917402e3a
Стоянов Данил <danya.bright@yandex.ru>
2019-04-24 16:07:02 +0300

    streamerController and streamerService methods for player creating

--- client/src/services/streamerService.js
+++ client/src/services/streamerService.js
@@ -2,25 +2,20 @@ import http from './httpService';
 import BasicService from './basicService';
 
 export default class StreamerService extends BasicService {
-  syncDataWithEscore = async() => {
-    const syncQuery = await http(`/api/system/sync`);
-    const syncResult = await syncQuery.json();
-    return syncResult;
-  }
-
-  addPlayerToRealTournament = async(tournamentId, player) => {
-    const playersQuery = await http('/api/admin/tournaments/real/players', {
-      method: 'PUT',
+  createPlayer = async({ name, photo, position }) => {
+    const request = await http(`/api/streamer/players`, {
+      method: 'POST',
       headers: {
-        Accept: 'application/json',
+        'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        tournamentId,
-        player,
+        name,
+        photo,
+        position
       }),
     });
-    const players = await playersQuery.json();
-    return players;
+    const requestResult = await request.json();
+    return requestResult;
   }
 }
--- server/controllers/streamerController.js
+++ server/controllers/streamerController.js
@@ -9,6 +9,30 @@ const StreamerController = () => {
     res.json({ players });
   });
 
+  router.post('/players', async (req, res) => {
+    const { name, photo, position } = req.body;
+
+    if(name.length > 20){
+      res.status(400).send({
+        error: 'Name can not contain more than 20 characters',
+      })
+    }
+
+    if(!position){
+      res.status(400).send({
+        error: 'Position field is required',
+      })
+    }
+
+    const player = await PlayerModel.create({
+      name,
+      photo,
+      position
+    });
+
+    res.send({ player });
+  });
+
   return router;
 }
 
