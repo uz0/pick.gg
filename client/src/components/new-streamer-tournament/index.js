283c0e48e6c4b72eb2cc0c2d91c103ceed58927f
Стоянов Данил <danya.bright@yandex.ru>
2019-04-29 10:29:06 +0300

    close streamer tournament feature

--- client/src/components/new-streamer-tournament/index.js
+++ client/src/components/new-streamer-tournament/index.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Modal from 'components/dashboard-modal';
 import MultiStepForm from './multi-step-form';
 import style from './style.module.css';
 
-
-class NewStreamerTournament extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <Modal
-        title={'Create new tournament'}
-        wrapClassName={style.tournament_modal}
-      >
-        <MultiStepForm />
-      </Modal>
-    );
-  }
-}
+const NewStreamerTournament = ({ closeModal }) => <Modal
+  title={'Create new tournament'}
+  wrapClassName={style.tournament_modal}
+  close={closeModal}
+>
+  <MultiStepForm />
+</Modal>
 
 export default NewStreamerTournament;
