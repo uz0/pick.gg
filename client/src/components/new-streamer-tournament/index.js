ad2160e257ac64e310519ee771316349771727ed
Ivan <kot91ivan@gmail.com>
2019-04-30 18:02:13 +0300

    Add variables for i18n

--- client/src/components/new-streamer-tournament/index.js
+++ client/src/components/new-streamer-tournament/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import Modal from 'components/dashboard-modal';
 import MultiStepForm from './multi-step-form';
 import style from './style.module.css';
+import i18n from 'i18n'
 
 const NewStreamerTournament = ({ closeModal }) => <Modal
-  title={'Create new tournament'}
+  title={i18n.t('create_new_tournament')}
   wrapClassName={style.tournament_modal}
   close={closeModal}
 >
