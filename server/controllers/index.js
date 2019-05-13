d5effbe40a384d86ac16fc876ca1ae61860c7f37
Стоянов Данил <danya.bright@yandex.ru>
2019-05-13 13:40:04 +0300

    working on server controllers refactoring

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,23 +1,15 @@
 import UsersController from "./usersController";
-import PingController from "./pingController";
 import AuthenticationController from "./authenticationController";
 import TournamentController from "./tournamentController";
-import RuleController from "./ruleController";
-import PlayerController from "./playerController";
 import TransactionController from "./transactionController";
 import SystemController from "./systemController";
 import StreamerController from "./streamerController";
 import AdminController from "./adminController";
-import PublicController from "./publicController";
 
 export {
-  PublicController,
   UsersController,
-  PingController,
   AuthenticationController,
   TournamentController,
-  RuleController,
-  PlayerController,
   TransactionController,
   SystemController,
   StreamerController,
---
+++ server/controllers/public/usersController.js
@@ -0,0 +1,83 @@
+import express from "express";
+import UserModel from "../../models/user";
+import TransactionModel from "../../models/transaction";
+
+let router = express.Router();
+
+const PublicUsersController = () => {
+  router.get('/', async (req, res) => {
+    const users = await UserModel.find();
+    res.json({ users });
+  });
+
+  router.get('/rating', async (req, res) => {
+    const usersWithWinnings = await TransactionModel.aggregate([
+      { 
+        $match: { origin: 'tournament winning' },
+      },
+      {
+        $group: { _id: "$userId", winning: { $sum: '$amount' } },
+      },
+      {
+        $lookup: { from: 'users', localField: '_id', foreignField: '_id', as: 'user' }
+      },
+      {
+        $unwind: '$user'
+      },
+      {
+        $project: {
+          _id: 1,
+          winning: 1,
+          'username': '$user.username',
+          'photo': '$user.photo',
+        }
+      },
+      {
+        $sort: {
+          winning: -1,
+        }
+      },
+    ])
+
+    const users = await UserModel.aggregate([
+      {
+        $project: {
+          _id: 1,
+          username: 1,
+          photo: 1,
+        }
+      },
+      {
+        $lookup: { from: 'transactions', localField: '_id', foreignField: 'userId', as: 'transactions' }
+      },
+      {
+        $addFields: {
+          winning: 0,
+        }
+      },
+      {
+        $match: { $expr: {$eq: [{$size: "$transactions"}, 0]} }
+      },
+      {
+        $project: {
+          transactions: 0,
+        }
+      }
+    ])
+
+    const rating = [...usersWithWinnings, ...users];
+
+    res.send({ rating });
+  });
+
+  router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const user = await UserModel.findOne({ _id: id });
+
+    res.json({ user });
+  });
+
+  return router;
+}
+
+export default PublicUsersController;
