361e64c7bf5b3d9f482d216820bc7b7e5bcbafbe
danilstoyanov <danya.bright@yandex.ru>
2019-05-14 22:12:29 +0300

    refreshed riot api key

--- server/riotFetch.js
+++ server/riotFetch.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
 const baseUrl = 'https://na1.api.riotgames.com/';
-const apiKey = 'RGAPI-c8e60b2d-e216-4e3e-ab96-a5b7d987db6a';
+const apiKey = 'RGAPI-420571e6-923c-41e5-bbcb-f14018d78d71';
 
 export default (url) => fetch(`${baseUrl}${url}?api_key=${apiKey}`);
