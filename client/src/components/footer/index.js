f4ec3296536a8798e2f58f02bdb07f17e750d5e5
Стоянов Данил <danya.bright@yandex.ru>
2019-05-15 13:00:18 +0300

    footer component refactoring

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,96 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { ReactComponent as DiscordIcon } from 'assets/icon-discord.svg';
 
 import style from './style.module.css';
 import i18n from 'i18n';
 
-class Footer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      profile: {
-        user: {},
-      },
-      locale: '',
-    };
-  }
-  async componentDidMount(){
-    const locale = localStorage.getItem('_pgg_locale');
-
-    this.setState({
-      locale,
-    });
-  }
-  changeLocale = (event) => {
-    this.setState({
-      locale: event.target.name,
-    });
-
+const Footer = () => {
+  const changeLocale = (event) => {
     localStorage.setItem('_pgg_locale', event.target.name);
-
     i18n.changeLanguage(event.target.name);
     window.location.reload();
   }
 
-  render() {
-    return (
-      <div className={style.footer}>
-        <div className={style.footer_wrap}>
-          <div className={style.one_row}>
-            <div className={style.info_footer}>
+  return <footer className={style.footer}>
+    <div className={style.container}>
+      <div className={style.info}>
+        <div className={style.copyright}>
+          <p>© 2019 uz0</p>
 
-              <p>© 2019 uz0</p>
+          <NavLink to="#">{i18n.t('terms_and_agreement')}</NavLink>
+        </div>
 
-              <NavLink to="#">{i18n.t('terms_and_agreement')}</NavLink>
-            </div>
+        <div className={style.contacts}>
+          <p>{i18n.t('contact_us')}:</p>
 
-            <div className={style.contact}>
-              <p>{i18n.t('contact_us')}:</p>
-
-              <NavLink to="#">
-                <DiscordIcon />
-              </NavLink>
-            </div>
-          </div>
-
-          {/* <div className={style.change_lng}>
-            <NavLink to="/ru">RU</NavLink>
-
-            <NavLink to="/en">EN</NavLink>
-          </div> */}
-          <div  className={style.change_lng}>
-                    <div className={style.item}>
-                      <label>Ru
-                      <input
-                        name='ru'
-                        type='radio'
-                        value={this.state.locale}
-                        checked={this.state.locale === 'ru'}
-                        onChange={this.changeLocale}
-                      />
-                      </label>
-                      
-                    </div>
-                    <div className={style.item}>
-                      <label>En
-                      <input
-                        name='en'
-                        type='radio'
-                        value={this.state.locale}
-                        checked={this.state.locale === 'en'}
-                        onChange={this.changeLocale}
-                      />
-                      </label>
-                      
-                    </div>
-                  </div>
-               
-
+          <NavLink to="#">
+            <DiscordIcon />
+          </NavLink>
         </div>
       </div>
-    );
-  }
+
+      <div className={style.lang_settings}>
+        <button onClick={changeLocale} name='ru'>RU</button>
+        <button onClick={changeLocale} name='en'>EN</button>
+      </div>
+    </div>
+  </footer>
 }
 
 export default Footer;
