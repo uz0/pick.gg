8fee968af027d3c6e98664da7745f670f119e18a
Стоянов Данил <danya.bright@yandex.ru>
2019-05-15 13:00:18 +0300

    footer component refactoring

--- client/src/locale.js
+++ client/src/locale.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import i18n from './i18n';
 
-class Locale extends React.Component {
+class Locale extends Component {
   componentDidMount(){
     const path = this.props.location.pathname;
     const locale = path === '/ru' ? 'ru' : 'en';
