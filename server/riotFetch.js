60c42d0cbd61b1ee3af3d3c75cda68962d211368
Стоянов Данил <danya.bright@yandex.ru>
2019-05-16 10:27:24 +0300

    riot apikey refresh

--- server/riotFetch.js
+++ server/riotFetch.js
@@ -2,6 +2,6 @@ import fetch from 'node-fetch';
 
 // const baseUrl = 'https://na1.api.riotgames.com/';
 const baseUrl = 'https://ru.api.riotgames.com';
-const apiKey = 'RGAPI-420571e6-923c-41e5-bbcb-f14018d78d71';
+const apiKey = 'RGAPI-0789a5fa-bc6c-4bba-82ac-95664a941f77';
 
 export default (url) => fetch(`${baseUrl}${url}?api_key=${apiKey}`);
