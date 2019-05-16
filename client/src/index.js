ace82531427eaaaef2c931ccc8e101d7301270a8
Стоянов Данил <danya.bright@yandex.ru>
2019-05-16 15:01:33 +0300

    installed and init yandex.metrika plugin for react app

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from './history';
+import { YMInitializer } from 'react-yandex-metrika';
 
 import 'typeface-roboto';
 import './index.css';
@@ -12,14 +13,17 @@ import Locale from './locale';
 // import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <Router basename='/' history={history}>
-    <Switch>
-      <Route exact path='/' component={Start} />
-      <Route exact path='/ru' component={Locale} />
-      <Route exact path='/en' component={Locale} />
-      <Route component={App} />
-    </Switch>
-  </Router>,
+  <>
+    <Router basename='/' history={history}>
+      <Switch>
+        <Route exact path='/' component={Start} />
+        <Route exact path='/ru' component={Locale} />
+        <Route exact path='/en' component={Locale} />
+        <Route component={App} />
+      </Switch>
+    </Router>
+    <YMInitializer accounts={[53679490]} />
+  </>,
   document.getElementById('root'),
 );
 
