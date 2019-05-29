055f65b0481c3a221deff7dd44c94ee2a9db083a
Стоянов Данил <danya.bright@yandex.ru>
2019-05-29 15:34:31 +0300

    reward card image replacement

--- client/src/components/reward-card/index.js
+++ client/src/components/reward-card/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import trophie from 'assets/trophie2.png';
+import { ReactComponent as Trophy } from 'assets/trophy.svg';
 
 import style from './style.module.css';
 
-const RewardCard = ({ description, key }) => {
+const RewardCard = ({ description }) => {
   return <div className={style.card}>
     <div className={style.content}>
-      <img src={trophie} alt='reward' />
+      <Trophy className={style.reward}/>
       <h3 className={style.name}>{description}</h3>
 
-      <Link to="#" className={style.claim}>Claim</Link>
+      <Link to='#' className={style.claim}>Claim</Link>
     </div>
   </div>;
 };
