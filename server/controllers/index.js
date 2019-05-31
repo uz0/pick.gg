1528204a9fedc5b7da1fde2a326b54db211a9445
Ermakoy <own3d.rus@gmail.com>
2019-05-31 16:52:57 +0300

    fix controller name

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,7 @@
 export { default as UsersController } from "./usersController";
 export { default as AuthenticationController } from "./authenticationController";
 export { default as TournamentController } from "./tournamentController";
-export { default as TransactionController } from "./transactionController";
+export { default as RewardController } from "./rewardController";
 export { default as SystemController } from "./systemController";
 export { default as StreamerController } from "./streamerController";
 export { default as AdminController } from "./adminController";
