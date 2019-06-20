038dcfb756fb9e7c9e087e2299a5c1754b864549
danilstoyanov <danya.bright@yandex.ru>
2019-06-20 21:43:11 +0300

    reward page rewards list rendering

--- client/src/pages/dashboard/rewards/index.js
+++ client/src/pages/dashboard/rewards/index.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import RewardCard from 'components/reward-card';
+import { http } from 'helpers';
+import { actions as modalActions } from 'components/modal-container';
+import actions from './actions';
+
+import style from './style.module.css';
 
 class Rewards extends Component {
-  state = {}
+  state = {
+    isLoading: false,
+  };
+
+  openRewardModal = () => this.props.toggleModal({ id: 'reward-modal' });
+
+  loadRewards = async () => {
+    this.setState({ isLoading: true });
+    const response = await http(`${document.location.origin}/api/admin/reward`);
+    const { rewards } = await response.json();
+    this.props.loadRewards(rewards);
+    this.setState({ isLoading: false });
+  };
+
+  async componentDidMount() {
+    if (!this.props.isLoaded) {
+      this.loadRewards();
+    }
+  }
 
   render() {
     return (
-      <h1>Hello rewards!</h1>
+      <div className={style.rewards}>
+        {this.props.rewardsList.map(({ _id, key, description, isClaimed }) => (
+          <RewardCard
+            key={_id}
+            rewardKey={key}
+            description={description}
+            isClaimed={isClaimed}
+          />
+        ))}
+      </div>
     );
   }
 }
 
-export default Rewards;
+export default compose(
+  connect(
+    state => ({
+      rewardsIds: state.rewards.ids,
+      rewardsList: state.rewards.list,
+      isLoaded: state.rewards.isLoaded,
+    }),
+
+    {
+      loadRewards: actions.loadRewards,
+      toggleModal: modalActions.toggleModal,
+    },
+  ),
+)(Rewards);
+
+export { default as actions } from './actions';
+export { default as reducers } from './reducers';
