9aba468706b6b18c2183586e8697330f2bb440f1
danilstoyanov <danya.bright@yandex.ru>
2019-06-21 19:55:25 +0300

    admin controller update

--- server/controllers/adminController/index.js
+++ server/controllers/adminController/index.js
@@ -1,20 +1,30 @@
 import express from 'express';
 
 import getRewards from './reward/get';
-import {validator as validateCreate, handler as createHandler} from './reward/create';
-import {validator as validateUpdate, handler as updateHandler} from './reward/update';
-import {validator as validateDelete, handler as deleteHandler} from './reward/delete';
+import {validator as validateRewardCreate, handler as createRewardHandler} from './reward/create';
+import {validator as validateRewardUpdate, handler as updateRewardHandler} from './reward/update';
+import {validator as validateRewardDelete, handler as deleteRewardHandler} from './reward/delete';
+
+import getUsers from './user/get';
+import {validator as validateUserCreate, handler as createUserHandler} from './user/create';
+import {validator as validateUserDelete, handler as deleteUserHandler} from './user/delete';
 
 let router = express.Router();
 
 const AdminController = () => {
   router.get('/reward', getRewards);
 
-  router.post('/reward', validateCreate, createHandler);
+  router.post('/reward', validateRewardCreate, createRewardHandler);
 
-  router.patch('/reward/:id', validateUpdate, updateHandler);
+  router.patch('/reward/:id', validateRewardUpdate, updateRewardHandler);
 
-  router.delete('/reward/:id', validateDelete, deleteHandler);
+  router.delete('/reward/:id', validateRewardDelete, deleteRewardHandler);
+
+  router.post('/user', validateUserCreate, createUserHandler);
+  
+  router.delete('/user/:id', validateUserDelete, deleteUserHandler);
+
+  router.get('/user', getUsers);
 
   return router;
 };
