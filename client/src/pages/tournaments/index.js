1a0536543f58d482561cf27a5913afc807e5f712
Ermakoy <own3d.rus@gmail.com>
2019-06-21 14:28:47 +0300

    refactor tournament page component

--- client/src/pages/tournaments/index.js
+++ client/src/pages/tournaments/index.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import compose from 'recompose/compose';
+import withHandlers from 'recompose/withHandlers';
 import { connect } from 'react-redux';
+
 import Button from 'components/button';
 import { actions as modalActions } from 'components/modal-container';
+
 import style from './style.module.css';
 
-class Tournaments extends Component {
-  openNewTournamentModal = () => this.props.toggleModal({ id: 'new-tournament-modal' });
-
-  render() {
-    return <div className={style.tournaments}>
-      <Button
-        appearance="_icon-accent"
-        icon="plus"
-        className={style.button}
-        onClick={this.openNewTournamentModal}
-      />
-    </div>;
-  }
-}
+const Tournaments = props => (
+  <div className={style.tournaments}>
+    <Button appearance="_icon-accent" icon="plus" className={style.button} onClick={props.openNewTournamentModal}/>
+  </div>
+);
 
 export default compose(
   connect(
@@ -28,4 +22,7 @@ export default compose(
       toggleModal: modalActions.toggleModal,
     },
   ),
+  withHandlers({
+    openNewTournamentModal: props => () => props.toggleModal({ id: 'new-tournament-modal' }),
+  })
 )(Tournaments);
