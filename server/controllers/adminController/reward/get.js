26563bc181a4614b7d57cc7dc70037a75388c457
danilstoyanov <danya.bright@yandex.ru>
2019-06-24 15:26:57 +0300

    indent fix

--- server/controllers/adminController/reward/get.js
+++ server/controllers/adminController/reward/get.js
@@ -1,7 +1,7 @@
 import RewardModel from '../../../models/reward';
 
 export default async (req, res) => {
-    const rewards = await RewardModel
-      .find();
-    res.json({ rewards });
+  const rewards = await RewardModel
+    .find();
+  res.json({ rewards });
 }
