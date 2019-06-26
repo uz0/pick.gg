f3d18656113804a562f7311f7a79db6f1847bed8
danilstoyanov <danya.bright@yandex.ru>
2019-06-26 17:01:54 +0300

    created reward modal

--- client/src/components/modal-container/reward/index.js
+++ client/src/components/modal-container/reward/index.js
@@ -1,81 +1,177 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Modal from 'components/modal';
-import { Formik, Form } from 'formik';
-import Input from 'components/form/input';
+import { connect } from 'react-redux';
+import { Form, Field, withFormik } from 'formik';
+import { FormInput } from 'components/form/input';
+import Button from 'components/button';
+import { compose } from 'recompose';
+import { http, getChangedFormFields } from 'helpers';
 import * as Yup from 'yup';
 import style from './style.module.css';
+import notificationActions from 'components/notification/actions';
+import actions from 'pages/dashboard/rewards/actions';
 
-const formSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
+import i18n from 'i18n';
 
-  username: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+const validationSchema = Yup.object().shape({
+  key: Yup.string().required('Key field is required!'),
 });
 
-class Reward extends Component {
-  submit = (values, actions) => {
-    console.log(values);
-    console.log(actions);
+const Reward = props => {
+  const modalTitle = props.options.isEditing ? 'Edit reward' : 'Add new reward';
+  const { reward } = props.options;
+
+  const deleteReward = async reward => {
+    const { _id } = reward;
+
+    console.log(reward, 'reward');
+
+    try {
+      await http(`/api/admin/reward/${_id}`, {
+        method: 'DELETE',
+      });
+
+      props.close();
+
+      props.deleteReward(_id);
+
+      props.showNotification({
+        type: 'success',
+        shouldBeAddedToSidebar: false,
+        message: i18n.t('notifications.success.entity_is_deleted', { name: reward.description }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  componentDidMount(){
-    console.log(this.props, 'this.props');
-  }
+  return (
+    <Modal
+      title={modalTitle}
+      close={props.close}
+      className={style.modal_content}
+    >
+      <Form>
+        <Field
+          component={FormInput}
+          label="Key"
+          name="key"
+          className={style.field}
+        />
 
-  render() {
-    return (
-      <Modal
-        title="New Tournament"
-        close={this.props.close}
-        className={style.modal_content}
-      >
-        <Formik
-          initialValues={this.props.options.reward}
-          validationSchema={formSchema}
-          onSubmit={this.submit}
-        >
-          {props => (
-            <Form>
-              <Input
-                label="Key"
-                name="key"
-                formProps={props}
-                className={style.field}
-              />
+        <Field
+          component={FormInput}
+          type="checkbox"
+          label="Is claimed"
+          name="isClaimed"
+          className={style.field}
+        />
 
-              <Input
-                type="checkbox"
-                label="Is claimed"
-                name="isClaimed"
-                formProps={props}
-                className={style.field}
-              />
+        <Field
+          component={FormInput}
+          label="Description"
+          name="description"
+          className={style.field}
+        />
 
-              <Input
-                label="Description"
-                name="description"
-                formProps={props}
-                className={style.field}
-              />
+        <Field
+          component={FormInput}
+          label="Image"
+          name="image"
+          className={style.field}
+        />
+        <button type="submit">Submit</button>
+        <Button
+          text="Delete"
+          appearance="_basic-danger"
+          onClick={() => deleteReward(reward)}
+        />
+      </Form>
+    </Modal>
+  );
+};
 
-              <Input
-                label="Image"
-                name="image"
-                formProps={props}
-                className={style.field}
-              />
+const enhance = compose(
+  connect(
+    null,
 
-              <button type="submit">Submit</button>
-            </Form>
-          )}
-        </Formik>
-      </Modal>
-    );
-  }
-}
+    {
+      showNotification: notificationActions.showNotification,
+      createReward: actions.createReward,
+      updateReward: actions.updateReward,
+      deleteReward: actions.deleteReward,
+    }
+  ),
+  withFormik({
+    validationSchema,
+    mapPropsToValues: ({ options }) => {
+      const { key, isClaimed, description, image } = options.reward;
+      return { key, isClaimed, description, image };
+    },
+    handleSubmit: async (values, formikBag) => {
+      const { isEditing } = formikBag.props.options;
+      const defaultState = formikBag.props.options.reward;
 
-export default Reward;
+      const editRewardRequest = async body => {
+        try {
+          const request = await http(`/api/admin/reward/${defaultState._id}`, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'PATCH',
+            body: JSON.stringify(body),
+          });
+
+          return request.json();
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      const createRewardRequest = async body => {
+        try {
+          const request = await http('/api/admin/reward', {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify(body),
+          });
+
+          return request.json();
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      if (isEditing) {
+        const changedFields = getChangedFormFields(defaultState, values);
+        const editRequest = await editRewardRequest(changedFields);
+
+        formikBag.props.close();
+
+        formikBag.props.showNotification({
+          type: 'success',
+          shouldBeAddedToSidebar: false,
+          message: i18n.t('notifications.success.entity_is_edited', { name: values.description }),
+        });
+
+        formikBag.props.updateReward(editRequest);
+      } else {
+        const request = await createRewardRequest(values);
+
+        formikBag.props.close();
+
+        formikBag.props.showNotification({
+          type: 'success',
+          shouldBeAddedToSidebar: false,
+          message: i18n.t('notifications.success.entity_is_created', { name: values.description }),
+        });
+
+        formikBag.props.updateReward(request);
+      }
+    },
+  }),
+);
+
+export default enhance(Reward);
