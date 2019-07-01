4c9e874f1ec16b529cbaf02bad9925a04dd7f4dd
danilstoyanov <danya.bright@yandex.ru>
2019-07-01 18:35:13 +0300

    Match model update

--- server/models/match.js
+++ server/models/match.js
@@ -3,24 +3,23 @@ const Schema = mongoose.Schema;
 
 const refTo = schemaName => ({ type: Schema.Types.ObjectId, ref: schemaName })
 
-let schema = new Schema({
-  tournamentId: refTo('Tournament'),
-  name: String,
-  playersResults: [{
-    userId: refTo('User'),
-    results: [{
-      ruleName: String,
-      value: Number
-    }]
-  }],
-  isActive: Boolean,
-  startedAt: Date,
-  endAt: Date,
-  updatedAt: Date,
-}, 
-{
-  toObject: { virtuals:true },
-}
-);
-
-export default mongoose.model('Match', schema);
+export default mongoose.model('Match',
+  new Schema({
+    tournamentId: refTo('Tournament'),
+    name: String,
+    playersResults: [{
+      userId: refTo('User'),
+      results: [{
+        ruleName: String,
+        value: Number
+      }]
+    }],
+    isActive: Boolean,
+    startedAt: Date,
+    endAt: Date,
+    updatedAt: Date,
+  },
+    {
+      toObject: { virtuals: true },
+    }
+  ));
