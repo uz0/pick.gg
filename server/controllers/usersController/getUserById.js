b2b590465d63ef6900bf41aa9331ae5924cf74ec
danilstoyanov <danya.bright@yandex.ru>
2019-07-02 13:21:56 +0300

    created get user by id handler

---
+++ server/controllers/usersController/getUserById.js
@@ -0,0 +1,28 @@
+import UserModel from '../../models/user';
+
+import { withValidationHandler } from '../helpers';
+import { param } from 'express-validator/check';
+import { isEntityExists } from '../validators';
+
+
+const validator = [
+  param('id')
+    .custom((value, req) => isEntityExists(value, UserModel))
+];
+
+const handler = withValidationHandler(async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await UserModel.findOne({ _id: userId })
+      .select('-password');
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      error
+    });
+  }
+});
+
+export { validator, handler };
