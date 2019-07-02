5018eb5efa9cdf45d71564205dce9e245bcb7073
danilstoyanov <danya.bright@yandex.ru>
2019-07-02 13:22:09 +0300

    created user controller

---
+++ server/controllers/usersController/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import {validator as getUserProfileValidator, handler as getUserProfile} from './getUserProfile';
+import {validator as userUpdateValidator, handler as updateUserHandler} from './updateUserProfile';
+
+import {validator as getUserByIdValidator, handler as getUserByIdHandler} from './getUserById';
+
+let router = express.Router();
+
+const UsersController = () => {
+  router.get('/me', getUserProfileValidator, getUserProfile);
+  
+  router.patch('/me', userUpdateValidator, updateUserHandler);
+
+  router.get('/:id', getUserByIdValidator, getUserByIdHandler);
+
+  return router;
+};
+
+export default UsersController;
