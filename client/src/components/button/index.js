b0e19da400b81ccc13f495b54ad2a738f073e4ee
danilstoyanov <danya.bright@yandex.ru>
2019-07-10 17:56:47 +0300

    add default type argument for button component

--- client/src/components/button/index.js
+++ client/src/components/button/index.js
@@ -5,16 +5,15 @@ import style from './style.module.css';
 
 const cx = classnames.bind(style);
 
-const Button = ({ type, text, onClick, icon, className, disabled, appearance }) => {
+const Button = ({ type = 'button', text, onClick, icon, className, disabled, appearance }) => {
   const isIconString = typeof icon === 'string';
   const isComponentIcon = icon && isIconString;
   const isPropIcon = icon && !isIconString;
-  const buttonType = type ? type : 'button';
 
   return (
     // eslint-disable-next-line react/button-has-type
     <button
-      type={buttonType}
+      type={type}
       className={cx('button', className)}
       appearance={appearance}
       disabled={disabled}
