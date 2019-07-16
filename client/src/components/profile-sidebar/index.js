539681332e3260ee2fba1f47cd6c8f29c643af0e
Ivan <kot91ivan@gmail.com>
2019-07-16 17:54:26 +0300

    Edited profile-sidebar component

--- client/src/components/profile-sidebar/index.js
+++ client/src/components/profile-sidebar/index.js
@@ -4,15 +4,34 @@ import style from './style.module.css';
 
 class ProfileSidebar extends Component {
   render() {
-    const { withData, nickname, description, source } = this.props;
+    const { nickname, description, summonerName, preferredPosition, source } = this.props;
     return (
       <aside className={style.sidebar}>
         <Avatar source={source}/>
 
         <div className={style.content}>
-          { !withData && <a href="/">Change avatar</a> }
-          { nickname && <div className={style.nickname}>{nickname}</div> }
-          { description && <p>{description}</p> }
+          {nickname && <div className={style.nickname}>{nickname}</div>}
+
+          {summonerName && (
+            <div className={style.wrap_info}>
+              <label className={style.label}>Summoner Name:</label>
+              <p className={style.text}>{summonerName}</p>
+            </div>
+          )}
+
+          {preferredPosition && (
+            <div className={style.wrap_info}>
+              <label className={style.label}>Position:</label>
+              <p className={style.text}>{preferredPosition}</p>
+            </div>
+          )}
+
+          {description && (
+            <div className={style.wrap_info}>
+              <label className={style.label}>About:</label>
+              <p className={style.text}>{description}</p>
+            </div>
+          )}
         </div>
       </aside>
     );
