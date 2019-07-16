5283f04630ec5f213c36e32903cf88d7ce6cdaa4
danilstoyanov <danya.bright@yandex.ru>
2019-07-16 20:52:07 +0300

    created tournament-rules widget

---
+++ client/src/components/tournament-rules/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import withProps from 'recompose/withProps';
+import classnames from 'classnames/bind';
+import Table from 'components/table';
+import Button from 'components/button';
+import { withCaptions } from 'hoc';
+import style from './style.module.css';
+
+import i18n from 'i18n';
+
+const cx = classnames.bind(style);
+
+const tableCaptions = ({ t, isMobile }) => ({
+  rule: {
+    text: t('rules'),
+    width: isMobile ? 75 : 200,
+  },
+
+  value: {
+    text: t('values'),
+    width: isMobile ? 25 : 30,
+  },
+});
+
+const renderRow = ({ className, itemClass, textClass, item, captions }) => {
+
+  console.log(className, 'className');
+
+  const valueStyle = { '--width': captions.value.width };
+  const ruleStyle = { '--width': captions.rule.width };
+
+  const ruleName = Object.keys(item)[0];
+
+  return (
+    <div key={ruleName} className={cx(className, style.row)}>
+      <div className={itemClass} style={ruleStyle}>
+        <span className={textClass}>{ruleName}</span>
+      </div>
+
+      <div className={itemClass} style={valueStyle}>
+        <span className={textClass}>{item[ruleName]}</span>
+      </div>
+    </div>
+  );
+};
+
+const Rules = ({ rules, captions, addRules }) => (
+  <div className={style.rules}>
+    <div className={style.header}>
+      <h3 className={style.subtitle}>{i18n.t('rules')}</h3>
+      {rules.length > 0 && (
+        <button
+          type="button"
+          className={style.button}
+        >
+          Edit
+        </button>
+      )}
+    </div>
+
+    {rules.length === 0 && (
+      <p className={style.empty}>{i18n.t('add_rules')}</p>
+    )}
+
+    <div className={style.content}>
+      {rules.length === 0 && (
+        <Button
+          appearance="_circle-accent"
+          icon="plus"
+          className={style.button}
+          onClick={addRules}
+        />
+      )}
+
+      {rules.length > 0 && (
+        <Table
+          noCaptions
+          captions={captions}
+          items={rules}
+          renderRow={renderRow}
+          isLoading={false}
+          className={style.table}
+          emptyMessage="There is no rules yet"
+        />
+      )}
+    </div>
+  </div>
+);
+
+export default compose(
+  connect(
+    (state, props) => ({
+      users: state.users.list,
+      tournament: state.tournaments.list[props.id],
+    }),
+  ),
+  withCaptions(tableCaptions),
+  withProps(props => {
+    const rules = Object.entries(props.tournament.rules).map(([key, value]) => ({ [key]: value }));
+
+    return {
+      ...props,
+      rules,
+    };
+  })
+)(Rules);
