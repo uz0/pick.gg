e07a43596f573c0bf157eb3d89a69d620fecc836
Ivan <kot91ivan@gmail.com>
2019-07-16 14:27:48 +0300

    Added 3 columns, buttons for show and hide

--- client/src/pages/rating/index.js
+++ client/src/pages/rating/index.js
@@ -19,13 +19,18 @@ const streamers = [
   { name: 'Rocketman', points: '1340' },
   { name: 'rabbit', points: '1700' },
   { name: 'SpiceFox', points: '1000' },
+  { name: 'Rox', points: '2' },
+  { name: 'Ata', points: '550' },
+  { name: 'agata', points: '17' },
+  { name: 'Rif', points: '250' },
+  { name: 'Wot oh my got cho proishodit skolko eche nado dobavit dannuh ', points: '234' },
 ];
 
 const ratingTableCaptions = {
 
   avatar: {
     text: '',
-    width: window.innerWidth < 480 ? 55 : 50,
+    width: window.innerWidth < 480 ? 40 : 70,
   },
 
   name: {
@@ -39,14 +44,20 @@ const sordStreamers = streamers.sort((a, b) => b.points - a.points);
 
 class Rating extends Component {
   state = {
-    playersList: [],
     isLoading: true,
+    columnStreamers: false,
   };
+
+  showStreamers = () => this.setState(prevState => ({ columnStreamers: !prevState.columnStreamers }));
+
+  showUsers = () => this.setState(prevState => ({ columnUsers: !prevState.columnUsers }));
+
+  showSummoners = () => this.setState(prevState => ({ columnSummoners: !prevState.columnSummoners }));
 
   renderRow = ({ className, itemClass, textClass, item }) => {
     const Avatar = () => item.photo ? <img src={item.photo} alt="userpic"/> : <AvatarPlaceholder/>;
     return (
-      <NavLink key={item._id} to={`/user/${item._id}`} className={cx(className)}>
+      <NavLink key={item._id} to={`/user/${item._id}`} className={cx(className, style.row_column)}>
 
         <div className={itemClass} style={{ '--width': ratingTableCaptions.avatar.width }}>
           <span className={cx(textClass, style.avatar_table)}><Avatar/></span>
@@ -61,10 +72,14 @@ class Rating extends Component {
 
   render() {
     console.log(sordStreamers);
+
     const topUsers = sordStreamers.slice(0, 3);
-    const sliceUsers = sordStreamers.slice(3);
-    console.log('3:', topUsers);
-    console.log('others:', sliceUsers);
+    const sliceUsers = sordStreamers.slice(3, 97);
+
+    const textStreamers = this.state.columnStreamers ? i18n.t('hide_streamers') : i18n.t('show_streamers');
+    const textUsers = this.state.columnUsers ? i18n.t('hide_users') : i18n.t('show_users');
+    const textSummoners = this.state.columnSummoners ? i18n.t('hide_summoners') : i18n.t('show_summoners');
+
     return (
       <div className={cx('container', 'rating')}>
 
@@ -79,11 +94,22 @@ class Rating extends Component {
             <Table
               captions={ratingTableCaptions}
               defaultSorting={this.tournamentsDefaultSorting}
-              items={streamers}
-              className={style.card}
+              items={topUsers}
+              className={style.column}
               renderRow={this.renderRow}
               isLoading={this.state.isLoading}
             />
+
+            <Table
+              captions={ratingTableCaptions}
+              defaultSorting={this.tournamentsDefaultSorting}
+              items={sliceUsers}
+              className={cx(style.hidden, { show: this.state.columnStreamers })}
+              renderRow={this.renderRow}
+              isLoading={this.state.isLoading}
+            />
+
+            <div className={style.see_all} onClick={this.showStreamers}>{textStreamers}</div>
           </div>
 
           <div className={cx(style.section)}>
@@ -95,11 +121,22 @@ class Rating extends Component {
             <Table
               captions={ratingTableCaptions}
               defaultSorting={this.tournamentsDefaultSorting}
-              items={streamers}
-              className={style.card}
+              items={topUsers}
+              className={style.column}
               renderRow={this.renderRow}
               isLoading={this.state.isLoading}
             />
+
+            <Table
+              captions={ratingTableCaptions}
+              defaultSorting={this.tournamentsDefaultSorting}
+              items={sliceUsers}
+              className={cx(style.hidden, { show: this.state.columnUsers })}
+              renderRow={this.renderRow}
+              isLoading={this.state.isLoading}
+            />
+
+            <div className={style.see_all} onClick={this.showUsers}>{textUsers}</div>
           </div>
 
           <div className={cx(style.section)}>
@@ -108,14 +145,26 @@ class Rating extends Component {
             </div>
 
             <h2>{i18n.t('Best summoners')}</h2>
+
             <Table
               captions={ratingTableCaptions}
               defaultSorting={this.tournamentsDefaultSorting}
-              items={streamers}
-              className={style.card}
+              items={topUsers}
+              className={style.column}
               renderRow={this.renderRow}
               isLoading={this.state.isLoading}
             />
+
+            <Table
+              captions={ratingTableCaptions}
+              defaultSorting={this.tournamentsDefaultSorting}
+              items={sliceUsers}
+              className={cx(style.hidden, { show: this.state.columnSummoners })}
+              renderRow={this.renderRow}
+              isLoading={this.state.isLoading}
+            />
+
+            <div className={style.see_all} onClick={this.showSummoners}>{textSummoners}</div>
           </div>
 
         </main>
