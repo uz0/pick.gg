d53f1e5989959916db45f8ca66627a4f7fe33be1
Ivan <kot91ivan@gmail.com>
2019-07-18 16:59:00 +0300

    Added render info from back

--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import { actions as storeActions } from 'store';
+import { actions as usersActions } from 'pages/dashboard/users';
+import { http } from 'helpers';
 
 import ProfileSidebar from 'components/profile-sidebar';
-import Preloader from '../../components/preloader';
+import Preloader from 'components/preloader';
+import thumb from 'assets/tournament_thumbnail.jpg';
 
 import style from './style.module.css';
 import i18n from 'i18n';
@@ -12,13 +15,41 @@ import classnames from 'classnames/bind';
 
 const cx = classnames.bind(style);
 
+const lastGames = [
+  { name: 'Fisrt figth', role: 'Streamer', position: 'Mid', place: '4 of 10' },
+  { name: '1vs1', role: 'Streamer', position: 'ADC', place: '1 of 2' },
+  { name: 'Super battle', role: 'User', position: 'Jungle', place: '5 of 10' },
+  { name: '3vs3', role: 'User', position: 'Mid', place: '2 of 6' },
+  { name: 'Super Mario', role: 'Streamer', position: 'Top', place: '8 of 10' },
+  { name: 'Super mid battle', role: 'User', position: 'Jungle', place: '2 of 10' },
+];
+
 class User extends Component {
   state = {
     loading: false,
   };
 
+  loadUser = async () => {
+    this.setState({ isLoading: true });
+    const userId = this.props.match.params.id;
+    const userRequest = await http(`/public/users/${userId}`);
+    const { user } = await userRequest.json();
+    this.props.loadUser([user]);
+    this.setState({ isLoading: false });
+  }
+
+  componentDidMount() {
+    if (!this.props.isLoaded) {
+      this.loadUser();
+    }
+  }
+
   render() {
-    const { about, imageUrl, summonerName, preferredPosition, username } = this.props.currentUser;
+    const userId = this.props.match.params.id;
+    const currentUser = this.props.users[userId] || {};
+
+    const { about, imageUrl, summonerName, preferredPosition, username } = currentUser;
+    const isGames = true;
 
     return (
       <div className={cx('container', 'user_page')}>
@@ -34,22 +65,51 @@ class User extends Component {
 
           <div className={style.user_statistics}>
             <div>
-              <h2>{i18n.t('scores')}</h2>
+              <h2>{i18n.t('Statistics')}</h2>
 
               <div className={style.statistics_masonry}>
                 <div className={style.item}>
-                  <div className={style.value}>0</div>
-                  <div className={style.key}>{i18n.t('rewards')}</div>
+                  <div className={style.key}>{i18n.t('Streamer')}</div>
+                  <div className={style.value}>4 of 95</div>
                 </div>
 
-                <div className={style.item}>
-                  <div className={style.value}>0</div>
-                  <div className={style.key}>{i18n.t('tournaments')}</div>
+                <div className={cx(style.item, { no_games: isGames })}>
+                  <div className={style.key}>{i18n.t('User')}</div>
+                  <div className={style.value}>No games</div>
                 </div>
+
+                <div className={cx(style.item, { no_games: isGames })}>
+                  <div className={style.key}>{i18n.t('Summoner')}</div>
+                  <div className={style.value}>No games</div>
+                </div>
+              </div>
+            </div>
+            <div className={style.last_games_wrap}>
+              <h2>{i18n.t('Last games')}</h2>
+
+              <div className={style.last_games}>
+                {lastGames && lastGames.map(item => (
+                  <div key={item} className={style.card}>
+                    <div className={style.img}>
+                      <img src={thumb}/>
+                    </div>
+
+                    <div className={style.content}>
+                      <div className={style.name}>{item.name}</div>
+                      <div className={style.role}>{item.role}</div>
+                      <div className={style.position}>{item.position}</div>
+                      <div className={style.place}>#{item.place}</div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
+
+        {this.state.isLoading &&
+          <Preloader/>
+        }
       </div>
     );
   }
@@ -57,12 +117,13 @@ class User extends Component {
 
 const enhance = compose(
   connect(
-    store => ({
-      currentUser: store.currentUser,
+    state => ({
+      users: state.users.list,
+      isLoaded: state.tournaments.isLoaded,
     }),
 
     {
-      setCurrentUser: storeActions.setCurrentUser,
+      loadUser: usersActions.loadUsers,
     }
   ),
 );
