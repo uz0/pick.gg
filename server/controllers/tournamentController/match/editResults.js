66f18a0c8f63858dc949ee581fe7a186da8dd336
danilstoyanov <danya.bright@yandex.ru>
2019-07-23 17:19:23 +0300

    match edit handler update

--- server/controllers/tournamentController/match/editResults.js
+++ server/controllers/tournamentController/match/editResults.js
@@ -1,5 +1,4 @@
 import { param, body } from 'express-validator/check';
-import uniqBy from 'lodash/uniqBy';
 
 import tournament from '../../../models/tournament';
 import match from '../../../models/match';
@@ -7,18 +6,9 @@ import match from '../../../models/match';
 import { isEntityExists } from '../../validators';
 import { withValidationHandler } from '../../helpers';
 
-import { normaliseResults } from './helpers';
-
 export const validator = [
   param('tournamentId').custom(id => isEntityExists(id, tournament)),
   param('matchId').custom(id => isEntityExists(id, match)),
-  param('matchId').custom(async (_, { req }) => {
-    const { tournamentId, matchId } = req.params;
-    const { matches } = await tournament.findById(tournamentId).exec();
-
-    if (!matches.includes(matchId))
-      throw new Error("Match don't exist on this tournament");
-  }),
   body()
     .not()
     .isEmpty()
@@ -26,18 +16,10 @@ export const validator = [
 
 export const handler = withValidationHandler(async (req, res) => {
   const { matchId } = req.params;
-  const rawResults = req.body;
-
-  const [$set, arrayFilters] = normaliseResults(rawResults).reduce(
-    ([set, arrayFilter], { userId, results }) => [
-      { ...set, [`results.$[${userId}].results`]: results },
-      { ...arrayFilter, [`${userId}.userId`]: userId }
-    ],
-    [{}, {}]
-  );
+  const results = req.body;
 
   const newMatch = await match
-    .findByIdAndUpdate(matchId, { $set }, { upsert: false, arrayFilters })
+    .findByIdAndUpdate(matchId, { $set: { playersResults: results }}, { new: true, upsert: false })
     .exec();
 
   res.json(newMatch);
