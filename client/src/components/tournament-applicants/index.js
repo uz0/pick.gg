54542e0b15da58288052b5a1d9e0bd812f91722a
danilstoyanov <danya.bright@yandex.ru>
2019-07-26 17:37:57 +0300

    tournament applicants widget feature

--- client/src/components/tournament-applicants/index.js
+++ client/src/components/tournament-applicants/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import compose from 'recompose/compose';
+import withProps from 'recompose/withProps';
+import withHandlers from 'recompose/withHandlers';
 import { connect } from 'react-redux';
 import classnames from 'classnames/bind';
+import pick from 'lodash/pick';
+import find from 'lodash/find';
+import debounce from 'lodash/debounce';
+import { http } from 'helpers';
+import { actions as tournamentsActions } from 'pages/tournaments';
 import Button from 'components/button';
 import Table from 'components/table';
 import { withCaptions } from 'hoc';
@@ -21,7 +28,7 @@ const tableCaptions = ({ t, isMobile }) => ({
   },
 });
 
-const renderRow = ({ className, itemClass, textClass, index, item, captions }) => {
+const renderRow = ({ className, itemClass, textClass, index, item, captions, props }) => {
   const numberStyle = { '--width': captions.number.width };
   const nameStyle = { '--width': captions.name.width };
 
@@ -32,43 +39,156 @@ const renderRow = ({ className, itemClass, textClass, index, item, captions }) =
       </div>
 
       <div className={itemClass} style={nameStyle}>
-        <span className={textClass}>{item.username}</span>
+        <span className={textClass}>{item.summonerName}{` ${item.status}`}</span>
       </div>
 
-      <Button appearance="_icon-transparent" icon="plus" className={style.action}/>
-      <Button appearance="_icon-transparent" icon="close" className={style.action}/>
+      {props.isCurrentUserCreator && (
+        <>
+          <Button
+            appearance="_icon-transparent"
+            icon="plus"
+            className={style.action}
+            onClick={props.acceptApplicant(item)}
+          />
+
+          <Button
+            appearance="_icon-transparent"
+            icon="close"
+            className={style.action}
+            onClick={props.rejectApplicant(item)}
+          />
+        </>
+      )}
     </div>
   );
 };
 
 const Applicants = ({
-  tournament,
+  applicants,
   captions,
   className,
+  applyTournament,
+  isCurrentUserCreator,
+  isAlreadyApplicantOrSummoner,
+  acceptApplicant,
+  rejectApplicant,
 }) => (
   <div className={cx(style.applicants, className)}>
     <div className={style.header}>
       <h3 className={style.subtitle}>Applicants</h3>
-      <button type="button" className={style.button}>Stop</button>
+      {!isAlreadyApplicantOrSummoner && (
+        <button type="button" className={style.button} onClick={debounce(applyTournament, 400)}>Apply as summoner</button>
+      )}
+      {isCurrentUserCreator && (
+        <button type="button" className={style.button}>Stop</button>
+      )}
     </div>
 
-    <Table
-      noCaptions
-      captions={captions}
-      items={tournament.applicants}
-      renderRow={renderRow}
-      className={style.table}
-      emptyMessage="There is no applicants yet"
-    />
+    <div className={style.content}>
+      <Table
+        noCaptions
+        captions={captions}
+        isCurrentUserCreator={isCurrentUserCreator}
+        items={applicants}
+        renderRow={renderRow}
+        className={style.table}
+        withProps={{
+          isCurrentUserCreator,
+          acceptApplicant,
+          rejectApplicant,
+        }}
+        emptyMessage="There is no applicants yet"
+      />
+    </div>
   </div>
 );
 
 export default compose(
-  withCaptions(tableCaptions),
-
   connect(
     (state, props) => ({
+      users: state.users.list,
+      currentUser: state.currentUser,
       tournament: state.tournaments.list[props.id],
     }),
+
+    {
+      updateTournament: tournamentsActions.updateTournament,
+    }
   ),
+  withCaptions(tableCaptions),
+  withProps(props => {
+    const users = Object.values(props.users);
+    const currentUserId = props.currentUser._id;
+    const isAlreadyApplicantOrSummoner = find(props.tournament.applicants, { user: currentUserId }) || props.tournament.summoners.includes(currentUserId);
+    const isCurrentUserCreator = props.tournament.creator._id === currentUserId;
+
+    if (props.tournament.applicants.length === 0) {
+      return {
+        ...props,
+        applicants: [],
+      };
+    }
+
+    const applicants = props.tournament.applicants.map(({ user, status }) => {
+      const applicant = users.find(item => item._id === user);
+
+      return {
+        ...pick(applicant, ['_id', 'summonerName']),
+        status,
+      };
+    });
+
+    return {
+      ...props,
+      applicants,
+      isAlreadyApplicantOrSummoner,
+      isCurrentUserCreator,
+    };
+  }),
+  withHandlers({
+    applyTournament: props => async () => {
+      const tournamentId = props.id;
+      const currentUserId = props.currentUser._id;
+
+      if (props.isAlreadyApplicantOrSummoner) {
+        alert('Вы уже подали заявку на участие в турнире');
+
+        return;
+      }
+
+      try {
+        await http(`/api/tournaments/${tournamentId}/attend`, { method: 'PATCH' });
+
+        props.updateTournament({
+          _id: tournamentId,
+          applicants: [...props.tournament.applicants, { user: currentUserId, status: 'PENDING' }],
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    acceptApplicant: props => item => async event => {
+      const tournamentId = props.id;
+      const applicantId = item._id;
+
+      if (item.status === 'ACCEPTED') {
+        alert(`${item.summonerName} is already accepted as summoner`);
+      }
+
+      try {
+        await http(`/api/tournaments/${tournamentId}/applicantStatus`, {
+          method: 'PATCH',
+          body: JSON.stringify({ status: 'ACCEPTED' }),
+        });
+
+        alert('В процессе');
+
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    rejectApplicant: props => async () => {
+
+    },
+  }),
 )(Applicants);
