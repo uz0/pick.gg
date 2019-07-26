25d0cc935557b43f4a7ba08ae79d57b0a95c7cd7
danilstoyanov <danya.bright@yandex.ru>
2019-07-26 22:08:20 +0300

    accept handler applicant fix

--- server/controllers/tournamentController/applicantStatus.js
+++ server/controllers/tournamentController/applicantStatus.js
@@ -2,23 +2,30 @@ import { param, check, body } from 'express-validator/check';
 
 import Tournament from '../../models/tournament';
 
-import { isEntityExists } from '../validators';
+import { isEntityExists, isUserHasToken } from '../validators';
 import { withValidationHandler } from '../helpers';
 
 const validator = [
   check().custom((value, { req }) => isUserHasToken(value, req)),
-  param('id')
-    .custom(id => isEntityExists(id, Tournament)),
-    body().not().isEmpty()
+  param('id').custom(id => isEntityExists(id, Tournament)),
 ];
 
 const handler = withValidationHandler(async (req, res) => {
   const { id } = req.params;
-  const { status, user } = req.body;
+  const { userId, newStatus } = req.body;
 
-  await Tournament
-    .findByIdAndUpdate(id, { [`applicants.$[${user}].status`]: status }, { upsert: false })
-    .exec();
+  try {
+    const newTournament = await Tournament
+      .update(
+        { _id: id },
+        { $set: { [`applicants.$[element].status`]: newStatus } },
+        { arrayFilters: [{ 'element.user': userId }] },
+      )
+      .exec();
+  } catch (error){
+    console.log(error);
+    res.status(400).send({})
+  }
 
   res.send({});
 });
