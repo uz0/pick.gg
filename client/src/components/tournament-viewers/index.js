1f74cf080f8abdf52f77d5b7a1d3a1b6f88d2473
danilstoyanov <danya.bright@yandex.ru>
2019-08-02 17:57:33 +0300

    viewer is winner add

--- client/src/components/tournament-viewers/index.js
+++ client/src/components/tournament-viewers/index.js
@@ -36,6 +36,8 @@ const renderRow = ({ className, itemClass, textClass, index, item, props: tourna
   const nameStyle = { '--width': captions.name.width };
   const pointsStyle = { '--width': captions.points.width };
 
+  const isViewerWinner = tournament.winners.find(winner => winner.id === item.userId);
+
   return (
     <div key={item.userId} className={cx(className, style.row)}>
       <div className={cx(itemClass, style.position)} style={numberStyle}>
@@ -43,7 +45,10 @@ const renderRow = ({ className, itemClass, textClass, index, item, props: tourna
       </div>
 
       <div className={itemClass} style={nameStyle}>
-        <span className={textClass}>{item.user && item.user.username}</span>
+        <span className={textClass}>
+          {item.user && item.user.username}
+          {isViewerWinner && <span className={style.is_winner}> is winner</span>}
+        </span>
       </div>
 
       {tournament.isStarted && (
