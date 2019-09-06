7ffe5e4544df0dfa2bae674fed9b04681ff338fc
Vasilisa Versus <dcversus@gmail.com>
2019-09-06 19:58:38 +0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -4,6 +4,4 @@ dotenv.config();
 export default {
   secret: process.env.SECRET || 'sdfmn43lkfvmsd',
   database: process.env.MONGODB_URI || 'mongodb://admin:admin@ds121248.mlab.com:21248/react-app',
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
 }
