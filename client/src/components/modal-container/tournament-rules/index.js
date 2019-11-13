0d330772de8310857ddf302ad036a40dc33942fc
Vladimir Ovechkin <ovchingus@gmail.com>
2019-11-13 19:23:25 +0300

    Separate rules logic

--- client/src/components/modal-container/tournament-rules/index.js
+++ client/src/components/modal-container/tournament-rules/index.js
@@ -1,44 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { compose } from 'recompose';
+import { compose, withProps } from 'recompose';
 import isEmpty from 'lodash/isEmpty';
-import { Form, Field, withFormik } from 'formik';
-import * as Yup from 'yup';
+import { withFormik } from 'formik';
+import { actions as tournamentsActions } from 'pages/tournaments';
+
+import Modal from 'components/modal';
 
 import { http } from 'helpers';
 
-import { FormInput } from 'components/form/input';
-import Modal from 'components/modal';
-
-import style from './style.module.css';
-import { actions as tournamentsActions } from 'pages/tournaments';
 import i18n from 'i18n';
 
-const validationSchema = Yup.object().shape({
-  kills: Yup.number()
-    .min(0)
-    .max(10)
-    .required('Required'),
-  deaths: Yup.number()
-    .min(0)
-    .max(10)
-    .required('Required'),
-  assists: Yup.number()
-    .min(0)
-    .max(10)
-    .required('Required'),
-});
+import style from './style.module.css';
+import * as LolRules from './lol';
+import * as PubgRules from './pubg';
 
 const AddRules = props => {
+  const { RulesForm } = props;
   return (
     <Modal
-      title={props.options.isEditing ? i18n.t('modal.edit_rules')  : i18n.t('modal.add_rules') }
+      title={props.options.isEditing ? i18n.t('modal.edit_rules') : i18n.t('modal.add_rules')}
       close={props.close}
       className={style.modal_content}
       wrapClassName={style.wrapper}
       actions={[
         {
-          text: props.options.isEditing ? i18n.t('edit') : i18n.t('add') ,
+          text: props.options.isEditing ? i18n.t('edit') : i18n.t('add'),
           type: 'button',
           appearance: '_basic-accent',
           onClick: props.submitForm,
@@ -46,37 +33,7 @@ const AddRules = props => {
         },
       ]}
     >
-      <Form>
-        <Field
-          component={FormInput}
-          className={style.field}
-          label="Kills"
-          name="kills"
-          type="number"
-          min="0"
-          max="10"
-        />
-
-        <Field
-          component={FormInput}
-          className={style.field}
-          label="Deaths"
-          name="deaths"
-          type="number"
-          min="0"
-          max="10"
-        />
-
-        <Field
-          component={FormInput}
-          className={style.field}
-          label="Assists"
-          name="assists"
-          type="number"
-          min="0"
-          max="10"
-        />
-      </Form>
+      <RulesForm/>
     </Modal>
   );
 };
@@ -91,21 +48,37 @@ const enhance = compose(
       updateTournament: tournamentsActions.updateTournament,
     }
   ),
+  withProps(props => {
+    if (props.tournament.game === 'LOL') {
+      return ({
+        RulesForm: LolRules.RulesForm,
+        validationSchema: LolRules.validationSchema,
+        defaultRules: LolRules.defaultRules,
+      });
+    }
+
+    if (props.tournament.game === 'PUBG') {
+      return ({
+        RulesForm: PubgRules.RulesForm,
+        validationSchema: PubgRules.validationSchema,
+        defaultRules: PubgRules.defaultRules,
+      });
+    }
+  }),
   withFormik({
-    validationSchema,
+    validationSchema: props => {
+      return props.validationSchema;
+    },
     mapPropsToValues: props => {
       if (isEmpty(props.tournament.rules)) {
-        return {
-          kills: 0,
-          deaths: 0,
-          assists: 0,
-        };
+        return props.defaultRules;
       }
 
       return props.tournament.rules;
     },
     handleSubmit: async (values, { props }) => {
       const { tournamentId } = props.options;
+      const { game } = props.tournament;
 
       try {
         await http(`/api/tournaments/${tournamentId}`, {
@@ -113,7 +86,7 @@ const enhance = compose(
             'Content-Type': 'application/json',
           },
           method: 'PATCH',
-          body: JSON.stringify({ rules: values }),
+          body: JSON.stringify({ rules: values, game }),
         });
 
         props.updateTournament({
