0eeefbc39b521cc8b61b8b2fa14fa24abee530ba
Roman <efim1382@gmail.com>
2019-12-12 12:36:38 +0300

    log mock user

--- client/src/index.js
+++ client/src/index.js
@@ -22,6 +22,7 @@ const isMobile = Boolean(md.mobile());
 store.dispatch(storeActions.setDevice(isMobile ? 'touch' : 'desktop'));
 
 const init = async () => {
+  console.log(process.env.MOCK_USER);
   if (process.env.MOCK_USER) {
     localStorage.setItem('auth-test-username', 'test1');
   }
