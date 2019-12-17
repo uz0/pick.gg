a1551aa6ac3f7f3a825e046a1b01aae812043167
Ivan <kot91ivan@gmail.com>
2019-12-17 22:22:17 +0200

    Added icon sword

--- client/src/components/tournament-card/index.js
+++ client/src/components/tournament-card/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import classnames from 'classnames/bind';
 
-import defaultBackground from 'assets/play-with.jpg';
+import { TOURNAMENT_IMAGES } from '../../constants';
 
 import Icon from 'components/icon';
+import SwordIcon from 'assets/icons/battle_flat.svg';
 
 import style from './style.module.css';
 
 const cx = classnames.bind(style);
 
+function arrayRandElement(arr) {
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+}
+
 const TournamentCard = ({ name, dateDay, dateMonth, people, className, imageUrl, description }) => {
-  const isBackground = imageUrl ? imageUrl : defaultBackground;
+  const isBackground = imageUrl ? imageUrl : arrayRandElement(TOURNAMENT_IMAGES);
   return (
-    <div style={{ backgroundImage: `url(${isBackground})` }} className={cx('card', className)}>
+    <div style={{ backgroundImage: `url(${imageUrl})`, }} className={cx('card', className)}>
       <div className={style.content}>
         <div>
           <div className={style.date}>
@@ -26,8 +32,14 @@ const TournamentCard = ({ name, dateDay, dateMonth, people, className, imageUrl,
           </div>
         </div>
 
-        <div style={style.basic}>
-          <h4 className={style.name}>{name}</h4>
+        <div className={style.basic}>
+          <div className={style.header}>
+            <div className={style.icon} title>
+              <img src={SwordIcon} title="test"/>
+            </div>
+            <h4 className={style.name}>{name}</h4>
+          </div>
+
           <p className={style.description}>{description}</p>
         </div>
       </div>
