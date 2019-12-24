aa2d2849291e417d139e8dc519e79795958f851d
Ivan <kot91ivan@gmail.com>
2019-12-24 14:26:23 +0200

    Added swagger definitions for users

--- server/controllers/usersController/index.js
+++ server/controllers/usersController/index.js
@@ -8,13 +8,149 @@ import { validator as userUpdateValidator, handler as updateUserHandler } from '
 
 import { validator as getUserByIdValidator, handler as getUserByIdHandler } from './getUserById';
 
-let router = express.Router();
+const router = express.Router();
+
+/**
+ * @swagger
+ *
+ * definitions:
+ *   User:
+ *     type: object
+ *     required:
+ *       - username
+ *       - email
+ *     properties:
+ *       username:
+ *         type: string
+ *       email:
+ *         type: string
+ *       imageUrl:
+ *         type: string
+ *       about:
+ *         type: string
+ *       isAdmin:
+ *         type: boolean
+ *       isVerified:
+ *         type: boolean
+ *       canProvideTournaments:
+ *         type: boolean
+ *       twitchAccount:
+ *         type: string
+ *       gameSpecificName:
+ *         type: object
+ *       contact:
+ *         type: string
+ *       regionId:
+ *         type: string
+ *       preferredPosition:
+ *         type: string
+ *         enum:
+ *         - ""
+ *         - "adc"
+ *         - "mid"
+ *         - "top"
+ *         - "jungle"
+ *         - "support"
+ */
 
 const UsersController = () => {
+/**
+ * @swagger
+ *
+ * /api/users/me:
+ *   get:
+ *     tags:
+ *     - "API(Users)"
+ *     summary: "Get user"
+ *     description: getUserProfile
+ *     produces:
+ *     - "application/xml"
+ *     - "application/json"
+ *     parameters:
+ *      - in: "body"
+ *        name: "body"
+ *        description: "Get user object"
+ *        required: true
+ *        schema:
+ *          type: object
+ *          $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: A user object.
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/User'
+ */
+
   router.get('/me', getUserProfileValidator, getUserProfile);
+
+  /**
+ * @swagger
+ *
+ * /api/users/me:
+ *   patch:
+ *     tags:
+ *     - API(Users)
+ *     description: userUpdateProfile
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *      - name: user
+ *        description: User object
+ *        in:  body
+ *        required: true
+ *        type: string
+ *        schema:
+ *          $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: A user object.
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/User'
+ */
+
   router.patch('/me', userUpdateValidator, updateUserHandler);
 
+  /**
+ * @swagger
+ *
+ * /api/users:
+ *   get:
+ *     tags:
+ *     - API(Users)
+ *     description: getAllUsers
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: A user array.
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ */
+
   router.get('/', getAllUsers);
+
+  /**
+ * @swagger
+ *
+ * /api/users/{id}:
+ *   get:
+ *     tags:
+ *     - API(Users)
+ *     description: getUserById
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: A user object.
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/User'
+ */
+
   router.get('/:id', getUserByIdValidator, getUserByIdHandler);
 
   return router;
