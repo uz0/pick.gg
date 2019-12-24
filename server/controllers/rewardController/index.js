269fe5a25ef462586fc439927b383aa84594eb01
Ivan <kot91ivan@gmail.com>
2019-12-24 14:26:52 +0200

    Added swagger definitions for rewards

--- server/controllers/rewardController/index.js
+++ server/controllers/rewardController/index.js
@@ -3,10 +3,77 @@ import express from 'express';
 import getUserRewards from './getUserRewards';
 import getStreamerRewards from './getStreamerRewards';
 
-let router = express.Router();
+const router = express.Router();
+
+/**
+ * @swagger
+ *
+ * definitions:
+ *   Reward:
+ *     type: object
+ *     properties:
+ *       key:
+ *         type: string
+ *       isClaimed:
+ *         type: boolean
+ *       description:
+ *         type: string
+ *       image:
+ *         type: string
+ */
 
 const RewardController = () => {
+  /**
+ * @swagger
+ *
+ * /api/reward:
+ *   get:
+ *     tags:
+ *     - "API(Reward)"
+ *     summary: "Get reward"
+ *     description: getUserRewards
+ *     parameters:
+ *      - in: "body"
+ *        name: "body"
+ *        description: "Get reward object"
+ *        required: true
+ *        schema:
+ *          type: object
+ *          $ref: '#/definitions/Reward'
+ *     responses:
+ *       200:
+ *         description: A reward object.
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Reward'
+ */
+
   router.get('/reward', getUserRewards);
+
+  /**
+ * @swagger
+ *
+ * /api/reward/streamer:
+ *   get:
+ *     tags:
+ *     - "API(Reward)"
+ *     summary: "Get reward streamer"
+ *     description: getStreamerRewards
+ *     parameters:
+ *      - in: "body"
+ *        name: "body"
+ *        description: "Get reward object"
+ *        required: true
+ *        schema:
+ *          type: object
+ *          $ref: '#/definitions/Reward'
+ *     responses:
+ *       200:
+ *         description: A reward object.
+ *         schema:
+ *           type: object
+ *           $ref: '#/definitions/Reward'
+ */
 
   router.get('/reward/streamer', getStreamerRewards);
 
