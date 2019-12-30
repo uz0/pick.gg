b8c020b8a43deb150c9fc84aa8ec9ffc232e4e83
Roman <efim1382@gmail.com>
2019-12-30 14:38:36 +0300

    fix user public page

--- client/src/components/profile-sidebar/index.js
+++ client/src/components/profile-sidebar/index.js
@@ -13,29 +13,33 @@ const GAMES = Object.keys(RULES);
 class ProfileSidebar extends Component {
   render() {
     const { nickname, description, gameSpecificName, source } = this.props;
+    const isContentShown = nickname || !isEmpty(gameSpecificName) || description;
+
     return (
       <aside className={style.sidebar}>
         <Avatar source={source} className={style.avatar}/>
 
-        <div className={style.content}>
-          {nickname && <div className={style.nickname}>{nickname}</div>}
+        {isContentShown && (
+          <div className={style.content}>
+            {nickname && <div className={style.nickname}>{nickname}</div>}
 
-          {!isEmpty(gameSpecificName) && GAMES.map(game =>
-            gameSpecificName[game] ? (
+            {!isEmpty(gameSpecificName) && GAMES.map(game =>
+              gameSpecificName[game] ? (
+                <div className={style.wrap_info}>
+                  <label className={style.label}>{i18n.t(`${game}_username`)}:</label>
+                  <p className={style.text}>{gameSpecificName[game]}</p>
+                </div>
+              ) : null
+            )}
+
+            {description && (
               <div className={style.wrap_info}>
-                <label className={style.label}>{i18n.t(`${game}_username`)}:</label>
-                <p className={style.text}>{gameSpecificName[game]}</p>
+                <label className={style.label}>{i18n.t('about')}:</label>
+                <p className={style.text}>{description}</p>
               </div>
-            ) : null
-          )}
-
-          {description && (
-            <div className={style.wrap_info}>
-              <label className={style.label}>{i18n.t('about')}:</label>
-              <p className={style.text}>{description}</p>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </aside>
     );
   }
