ba43d5f8c6156a0cff247791b40202f284abc2e1
danilstoyanov <danya.bright@yandex.ru>
2020-01-10 01:53:58 +0300

    ---WIP--- updated add match modal

--- client/src/components/modal-container/add-match/index.js
+++ client/src/components/modal-container/add-match/index.js
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Form, Field, withFormik } from 'formik';
-import * as Yup from 'yup';
-import isEmpty from 'lodash/isEmpty';
 import { compose } from 'recompose';
+import omit from 'lodash/omit';
+import values from 'lodash/values';
+import flatten from 'lodash/flatten';
+import get from 'lodash/get';
+import pick from 'lodash/pick';
+import uuid from 'uuid';
 import { actions as tournamentsActions } from 'pages/tournaments';
 
-import Select from 'components/form/selects/team-select';
-import { FormInput } from 'components/form/input';
+import modalActions from 'components/modal-container/actions';
+import Input from 'components/form/input';
 import Modal from 'components/modal';
+import Button from 'components/button';
 
-import { http } from 'helpers';
+// Import { http } from 'helpers';
 
 import i18n from 'i18n';
 
+import Roster from './roster';
 import style from './style.module.css';
 
-const genericSchema = Yup.object().shape({
-  name: Yup.string().required('Required'),
-});
-
-const lolSchema = genericSchema.shape({
-  teams: Yup.object().shape({
-    red: Yup.string()
-      .required('Required')
-      .notOneOf([Yup.ref('blue')], 'You can\'t choose same teams'),
-    blue: Yup.string()
-      .required('Required')
-      .notOneOf([Yup.ref('red')], 'You can\'t choose same teams'),
-  }),
-});
-
-const setValidationSchema = game => game === 'LOL' ? lolSchema : genericSchema;
-
 const Match = props => {
+  const [rosters, setRosters] = useState({});
+
+  // Const isSubmitButtonDisabled = !props.dirty || !isEmpty(props.errors) || isEmpty(firstTeamPlayers) || isEmpty(secondTeamPlayers) || props.isSubmitting;
+
+  const addRoster = () => {
+    const rosterId = uuid();
+    openChoosePlayersModal(rosterId);
+  };
+
+  const updateRosters = (rosterId, rosterPlayers) => {
+    const updatedRosters = {
+      ...rosters,
+      [rosterId]: rosterPlayers,
+    };
+
+    setRosters(updatedRosters);
+  };
+
+  const deleteRoster = rosterId => {
+    const updatedRosters = omit(rosters, rosterId);
+
+    setRosters(updatedRosters);
+  };
+
+  const openChoosePlayersModal = rosterId => {
+    const disabledPlayers = flatten(values(omit(rosters, rosterId)));
+    const selectedPlayers = get(pick(rosters, rosterId), rosterId, []);
+
+    props.toggleModal({
+      id: 'add-summoners-modal',
+
+      options: {
+        tournamentId: props.tournament._id,
+        disabledPlayers,
+        selectedPlayers,
+        action: rosterPlayers => updateRosters(rosterId, rosterPlayers),
+      },
+    });
+  };
+
   return (
     <Modal
       title={i18n.t('add_match')}
@@ -46,41 +74,43 @@ const Match = props => {
           type: 'submit',
           appearance: '_basic-accent',
           onClick: props.submitForm,
-          disabled: !props.dirty || !isEmpty(props.errors) || props.isSubmitting,
+          disabled: false,
         },
       ]}
     >
-      <Form>
-        <Field
-          name="name"
-          placeholder="Enter match name"
-          component={FormInput}
-          label={i18n.t('match_name')}
-          className={style.field}
-        />
+      <Input
+        name="name"
+        placeholder="Enter match name"
+        label={i18n.t('match_name')}
+        className={style.field}
+      />
 
-        {props.tournament.game === 'LOL' && (
-          <>
-            <Field
-              name="teams.red"
-              label="Blue team"
-              placeholder="Choose blue team"
-              component={Select}
-              options={props.teams}
-              className={style.field}
-            />
+      {props.tournament.game === 'LOL' && props.tournament.teams.length === 1 && (
+        <div className={style.rosters}>
+          <div className={style.title}>Играют:</div>
 
-            <Field
-              name="teams.blue"
-              label="Red team"
-              placeholder="Choose red team"
-              component={Select}
-              options={props.teams}
-              className={style.field}
-            />
-          </>
-        )}
-      </Form>
+          <div className={style.list}>
+            {Object.entries(rosters).map(([key, value], index) => (
+              <Roster
+                key={key}
+                rosterId={key}
+                title={`Team ${index + 1}`}
+                game={props.tournament.game}
+                players={value}
+                onDelete={deleteRoster}
+                onEdit={openChoosePlayersModal}
+              />
+            ))}
+          </div>
+
+          <Button
+            appearance="_small-accent"
+            text="Add roster"
+            className={style.button}
+            onClick={addRoster}
+          />
+        </div>
+      )}
     </Modal>
   );
 };
@@ -93,57 +123,45 @@ const enhance = compose(
     }),
     {
       updateTournament: tournamentsActions.updateTournament,
+      toggleModal: modalActions.toggleModal,
     }
   ),
-  withFormik({
-    validationSchema: props => setValidationSchema(props.tournament.game),
-    mapPropsToValues: props => {
-      const values = {
-        LOL: {
-          name: '',
-          teams: {
-            red: '',
-            blue: '',
-          },
-        },
-        PUBG: {
-          name: '',
-        },
-      };
+  // WithFormik({
+  //   validationSchema: genericSchema,
+  //   mapPropsToValues: () => ({
+  //     name: '',
+  //     rosters: {},
+  //     teams: [],
+  //   }),
+  //   handleSubmit: async (values, { props }) => {
+  //     const { tournamentId } = props.options;
 
-      return values[props.tournament.game];
-    },
-    handleSubmit: async (values, { props }) => {
-      const { tournamentId } = props.options;
-      const { game } = props.tournament;
+  //     try {
+  //       const matchRequest = await http(`/api/tournaments/${tournamentId}/matches`, {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         method: 'POST',
+  //         body: JSON.stringify({
+  //           tournamentId,
+  //           ...values,
+  //         }),
+  //       });
 
-      try {
-        const matchRequest = await http(`/api/tournaments/${tournamentId}/matches`, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-          body: JSON.stringify({
-            game,
-            tournamentId,
-            ...values,
-          }),
-        });
+  //       const newMatch = await matchRequest.json();
+  //       const matches = [...props.tournament.matches, newMatch];
 
-        const newMatch = await matchRequest.json();
-        const matches = [...props.tournament.matches, newMatch];
+  //       props.updateTournament({
+  //         ...props.tournament,
+  //         matches,
+  //       });
 
-        props.updateTournament({
-          ...props.tournament,
-          matches,
-        });
-
-        props.close();
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  })
+  //       props.close();
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   },
+  // })
 );
 
 export default enhance(Match);
